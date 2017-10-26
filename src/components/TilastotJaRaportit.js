2dfaaee0b843ca4ecba11645f4a9a8949a399ce9
Samu Karjalainen <samu.karjalainen@arcusys.fi>
2017-10-26 10:43:10 +0300

    Tilastot ja raportit komponentti lisätty

---
+++ src/components/TilastotJaRaportit.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import { Helmet } from 'react-helmet'
+
+class TilastotJaRaportit extends Component {
+  render() {
+    return (
+      <div>
+        <Helmet>
+          <title>Tilastot ja raportit | Oiva</title>
+        </Helmet>
+        <p>Tilastot ja raportit</p>
+      </div>
+    )
+  }
+}
+
+export default TilastotJaRaportit
