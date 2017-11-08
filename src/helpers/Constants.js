4b7d36800f2a46d314c059e6551b46696ffe9cf4
Aki Heikkinen <aki.heikkinen@arcusys.fi>
2017-11-08 10:41:26 +0200

    URL-osoitteet päivitetty

--- src/helpers/Constants.js
+++ src/helpers/Constants.js
@@ -1,5 +1,5 @@
-export const API_BASE_URL = 'http://localhost:8099/api'
-export const HTTP_HOST_BASE_URL = 'http://localhost:3000'
+export const API_BASE_URL = 'https://localhost/api'
+export const HTTP_HOST_BASE_URL = 'https://localhost'
 export const CAS_LOGIN_REDIRECT_URL = `${API_BASE_URL}/auth/login?redirect=${HTTP_HOST_BASE_URL}/cas-ready`
 export const CAS_LOGIN_READY = `${HTTP_HOST_BASE_URL}/cas-ready`
 export const APP_WIDTH = 1280
