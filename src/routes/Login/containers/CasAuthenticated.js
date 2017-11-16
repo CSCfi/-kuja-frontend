820d25490cedfe2f3ac15d3c1681be3eb133a398
Samu Karjalainen <samu.karjalainen@arcusys.fi>
2017-11-16 13:19:15 +0200

    Cas-autentikointi näyttämään onnistuminen tai virhe

--- src/routes/Login/containers/CasAuthenticated.js
+++ src/routes/Login/containers/CasAuthenticated.js
@@ -4,23 +4,27 @@ import { connect } from 'react-redux'
 import { getRoles } from 'routes/Login/modules/user'
 
 class CasAuthenticated extends Component {
-  componentWillMount() {    
+  componentWillMount() {
     this.props.getRoles()
   }
 
   render() {
     return (
       <div>
-        <p>CAS-autentikointi valmis</p>
+        {this.props.user.hasErrored ? <p>Autentikoinnissa tapahtui virhe</p> : <p>CAS-autentikointi valmis</p>}
       </div>
     )
   }
 }
 
-function mapDispatchToProps (dispatch) {
+function mapStateToProps(state) {
+  return { user: state.user }
+}
+
+function mapDispatchToProps(dispatch) {
   return {
     getRoles: () => dispatch(getRoles())
   }
 }
 
-export default connect(null, mapDispatchToProps)(CasAuthenticated)
+export default connect(mapStateToProps, mapDispatchToProps)(CasAuthenticated)
