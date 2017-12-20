d9db82a1d2a1e064e4f1d7fa34e390d6daa9811e
Samu Karjalainen <samu.karjalainen@arcusys.fi>
2017-12-20 15:34:58 +0200

    Muutetaan sana 'peruskoulutus' --> 'koulutus' muotoon

--- src/routes/Home/components/InfoBox.js
+++ src/routes/Home/components/InfoBox.js
@@ -5,7 +5,7 @@ import { COLORS } from 'modules/styles'
 
 const InfoBox = styled.div`
   margin: 20px;
-  padding: 20px;
+  padding: 20px 40px;
   background-color: rgba(237, 237, 237, 0.5);
   width: 100%;
   max-width: ${props => props.maxWidth ? props.maxWidth : '100%'};
