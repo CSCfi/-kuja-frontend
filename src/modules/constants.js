2348d104cd1558756dfd0c2f50b49f7933146158
Samu Karjalainen <samu.karjalainen@arcusys.fi>
2018-01-17 17:25:44 +0200

    Poikkeusluvat näytetään oikein julkisessa profiilissa

--- src/modules/constants.js
+++ src/modules/constants.js
@@ -1,10 +1,13 @@
-// NETWORKING
+// Palvelu
 export const HOST_BASE_URL = window.location.origin
 export const API_BASE_URL = `${HOST_BASE_URL}/api`
 export const CAS_LOGIN_REDIRECT_URL = `${API_BASE_URL}/auth/login?redirect=${HOST_BASE_URL}/cas-ready`
 export const CAS_LOGIN_READY = `${HOST_BASE_URL}/cas-ready`
 export const CAS_BASE_URL = 'https://testi.virkailija.opintopolku.fi'
 export const CAS_LOGOUT_URL = `${CAS_BASE_URL}/cas/logout?service=${HOST_BASE_URL}`
+
+// Luvan erikoiskäsittely
+export const LUPA_EXCEPTION_PATH = `${API_BASE_URL}/pebble/resources/liitteet/lisakoulutusluvat/`
 
 // USERS & ROLES
 export const ROLE_APPLICATION = 'APP_KOUTE'
