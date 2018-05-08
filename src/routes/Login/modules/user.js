7f66297bc9d1de5179be2d30a55266fa09f5b4d5
Jari Saarela <jari.saarela@arcusys.fi>
2018-05-08 13:42:57 +0300

    CSCOIVA-441: Automaattinen siirtyminen KJ:n tai esittelijän omalle sivulle kirjautumisesta.

--- src/routes/Login/modules/user.js
+++ src/routes/Login/modules/user.js
@@ -16,12 +16,16 @@ export const LOGOUT_USER_START = 'LOGOUT_USER_START'
 export const LOGOUT_USER_SUCCESS = 'LOGOUT_USER_SUCCESS'
 export const LOGOUT_USER_FAILURE = 'LOGOUT_USER_FAILURE'
 
+export const GET_ORGANISATION_START = 'GET_ORGANISATION_START'
+export const GET_ORGANISATION_SUCCESS = 'GET_ORGANISATION_SUCCESS'
+export const GET_ORGANISATION_FAILURE = 'GET_ORGANISATION_FAILURE'
+
 // Actions
 export function getRoles() {
   return (dispatch) => {
     dispatch({ type: LOGIN_GET_ROLES_START })
     
-    axios.get(`${API_BASE_URL}/auth/me`, { withCredentials: true })
+    return axios.get(`${API_BASE_URL}/auth/me`, { withCredentials: true })
       .then((response) => {
 
           var userObj = JSON.parse(JSON.stringify(response.data))
@@ -45,6 +49,22 @@ export function getRoles() {
       .catch((err) => dispatch({ type: LOGIN_GET_ROLES_FAILURE, payload: err }))
       
   }
+}
+
+export function getOrganisation(oid) {
+    return (dispatch) => {
+        dispatch({ type: GET_ORGANISATION_START })
+        const request = fetch(`${API_BASE_URL}/organisaatiot/${oid}`)
+
+        request
+            .then((response) => response.json())
+            .then((data) => {
+                dispatch({ type: GET_ORGANISATION_SUCCESS, payload: data })
+            })
+            .catch((err) => dispatch({ type: GET_ORGANISATION_FAILURE, payload: err }))
+
+    }
+
 }
 
 export function logoutUser() {
@@ -74,6 +94,7 @@ export function dummyLoginUser(values) {
 export const actions = {
   getRoles,
   logoutUser,
+  getOrganisation,
   dummyLogoutUser,
   dummyLoginUser
 }
@@ -81,30 +102,61 @@ export const actions = {
 // Action handlers
 const ACTION_HANDLERS = {
   [LOGIN_GET_ROLES_START] : (state, action) => {
-    return {
-      ...state,
-      isFetching: true,
-      fetched: false,
-      hasErrored: false
-    }
-  },
-  [LOGIN_GET_ROLES_SUCCESS] : (state, action) => {
-    return {
-      ...state,
-      user: action.payload,
-      isFetching: false,
-      fetched: true,
-      hasErrored: false
-    }
-  },
-  [LOGIN_GET_ROLES_FAILURE] : (state, action) => {
-    return {
-      ...state,
-      isFetching: false,
-      fetched: false,
-      hasErrored: true
-    }
-  },
+        return {
+            ...state,
+            isFetching: true,
+            fetched: false,
+            hasErrored: false
+        }
+    },
+    [LOGIN_GET_ROLES_SUCCESS] : (state, action) => {
+        return {
+            ...state,
+            user: action.payload,
+            isFetching: false,
+            fetched: true,
+            hasErrored: false
+        }
+    },
+    [LOGIN_GET_ROLES_FAILURE] : (state, action) => {
+        return {
+            ...state,
+            isFetching: false,
+            fetched: false,
+            hasErrored: true
+        }
+    },
+    [GET_ORGANISATION_START] : (state, action) => {
+        return {
+            ...state,
+            oppilaitos: {
+                isFetching: true,
+                fetched: false,
+                hasErrored: false
+            }
+        }
+    },
+    [GET_ORGANISATION_SUCCESS] : (state, action) => {
+        return {
+            ...state,
+            oppilaitos: {
+                organisaatio: action.payload,
+                isFetching: false,
+                fetched: true,
+                hasErrored: false
+            }
+        }
+    },
+    [GET_ORGANISATION_FAILURE] : (state, action) => {
+        return {
+            ...state,
+            oppilaitos: {
+                isFetching: true,
+                fetched: false,
+                hasErrored: false
+            }
+        }
+    },
   [LOGOUT_USER_START] : (state, action) => {
     return {
       ...state,
