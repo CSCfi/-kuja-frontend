c92db3a3ab0cc2da0c5bf54a3e9573a9bab46674
Sami Heikkinen <sami@Idap-sheikkinen.local>
2019-01-15 12:42:17 +0200

    Lupahistoria muutettu taulukoksi

--- src/routes/Jarjestajat/Jarjestaja/components/LupaHistory.js
+++ src/routes/Jarjestajat/Jarjestaja/components/LupaHistory.js
@@ -1,12 +1,15 @@
 import _ from 'lodash'
 import React, { Component } from 'react'
+import Media from 'react-media'
 import styled from 'styled-components'
+import { Table, Thead, Tbody, Th, Tr } from "../../../../modules/Table"
+import { MEDIA_QUERIES } from "../../../../modules/styles"
 
 import LupaHistoryItem from './LupaHistoryItem'
 import Loading from '../../../../modules/Loading'
 
-const LupaHistoryWrapper = styled.div`
-  margin: 20px 0;
+const WrapTable = styled.div`
+   padding-bottom: 200px;
 `
 
 class LupaHistory extends Component {
@@ -22,7 +25,38 @@ class LupaHistory extends Component {
     const { fetched, isFetching, hasErrored, data } = this.props.lupaHistory
 
     if (fetched) {
-      return <LupaHistoryWrapper>{this.renderLupaHistoryList(data)}</LupaHistoryWrapper>
+      return (
+        <WrapTable>
+          <Media query={MEDIA_QUERIES.MOBILE} render={() =>
+            <Table>
+              <Thead>
+              <Tr>
+                <Th>Voimassa olevat luvat</Th>
+              </Tr>
+              </Thead>
+              <Tbody>
+                {this.renderLupaHistoryList(data)}
+              </Tbody>
+            </Table>
+          }/>
+          <Media query={MEDIA_QUERIES.TABLET_MIN} render={() =>
+            <Table>
+              <Thead>
+              <Tr>
+                <Th>Diaarinumero</Th>
+                <Th>Päätöspvm</Th>
+                <Th>Voimaantulopvm</Th>
+                <Th>Päättymispvm</Th>
+                <Th>Kumottu</Th>
+              </Tr>
+              </Thead>
+              <Tbody>
+                {this.renderLupaHistoryList(data)}
+              </Tbody>
+            </Table>
+          }/>
+        </WrapTable>
+      )
     } else if (isFetching) {
       return <Loading />
     } else if (hasErrored) {
--- src/routes/Jarjestajat/Jarjestaja/components/LupaHistoryItem.js
+++ src/routes/Jarjestajat/Jarjestaja/components/LupaHistoryItem.js
@@ -4,52 +4,31 @@ import Moment from 'react-moment'
 
 import { MEDIA_QUERIES } from "../../../../modules/styles"
 import { API_BASE_URL } from "../../../../modules/constants"
+import { Td, Tr } from "../../../../modules/Table"
 
 import pdf from 'static/images/icon-pdf-small.png'
-
-const LupaHistoryItemWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  margin: 15px 0;
-`
-
-const LupaText = styled.span`
-  margin: 3px 0 0 6px;
-  
-  @media ${MEDIA_QUERIES.MOBILE} {
-    display: flex;
-    flex-direction: column;
-  }
-`
-
-const TextPartial = styled.span`
-  margin-right: 10px;
-`
 
 const LupaHistoryItem = (props) => {
   const { filename, diaarinumero, voimassaoloalkupvm, voimassaololoppupvm, paatospvm } = props.lupaHistoria
 
   return (
     <a href={`${API_BASE_URL}/pebble/resources/liitteet/lupahistoria/${filename}`} target="_blank">
-      <LupaHistoryItemWrapper>
-        <img src={pdf} alt="Päättynyt lupa"/>
-        <LupaText>
-          <TextPartial>Diaarinumero: {diaarinumero}</TextPartial>
-          <TextPartial>
-            Paatos tehty:&nbsp;
+    <Tr>
+          <Td>{diaarinumero}</Td>
+          <Td>
             <Moment format="DD.MM.YYYY">{paatospvm}</Moment>
-          </TextPartial>
+          </Td>
+          <Td>
+            <Moment format="DD.MM.YYYY">{voimassaoloalkupvm}</Moment>
+          </Td>
+          <Td>
+            <Moment format="DD.MM.YYYY">{voimassaololoppupvm}</Moment>
+          </Td>
           { voimassaoloalkupvm === "2018-01-01" && voimassaololoppupvm === "2018-01-01"
-              ? <TextPartial>Kumottu: 12.12.2017</TextPartial>
-              : (
-                <TextPartial>Voimassa:&nbsp;
-                  <Moment format="DD.MM.YYYY">{voimassaoloalkupvm}</Moment>
-                  &nbsp;-&nbsp;
-                  <Moment format="DD.MM.YYYY">{voimassaololoppupvm}</Moment>
-                </TextPartial>)
+              ? <Td>Kumottu: 12.12.2017</Td>
+              : <Td></Td>
           }
-        </LupaText>
-      </LupaHistoryItemWrapper>
+      </Tr>
     </a>
   )
 }
