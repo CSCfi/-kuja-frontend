b2b730f3c87822f1dd6792de5dbf90215acf3902
Sami Heikkinen <sami.heikkinen@valamis.com>
2019-03-05 10:45:24 +0200

    Korjattu merge

--- src/routes/Jarjestajat/Jarjestaja/Hakemukset/Muutospyynto/components/Muutos.js
+++ src/routes/Jarjestajat/Jarjestaja/Hakemukset/Muutospyynto/components/Muutos.js
@@ -74,8 +74,8 @@ class Muutos extends Component {
   render() {
     const { isHidden } = this.state
     const { muutokset, muutos, sisaltaa_merkityksen, fields, kategoria } = this.props
-    const { koodiarvo, type, meta, muutosperustelukoodiarvo, koodisto, nimi, label, arvo, parentId } = muutos
-    const { perusteluteksti, perusteluteksti_oppisopimus, perusteluteksti_vaativa, perusteluteksti_tyovoima } = meta
+    const { koodiarvo, type, meta, muutosperustelukoodiarvo, koodisto, label, arvo, parentId } = muutos
+    const { perusteluteksti, perusteluteksti_oppisopimus, perusteluteksti_vaativa, perusteluteksti_tyovoima, nimi } = meta
     const { perusteluteksti_vankila, perusteluteksti_kuljetus_perus, perusteluteksti_kuljetus_jatko} = meta
 
     const helpText = "Perustele lyhyesti miksi tälle muutokselle on tarvetta"
