8889f0fccef995cfbc1f77dc9c6a1dcc238943e3
Raimo Pitkänen <rapitkan@gmail.com>
2019-04-10 23:06:52 +0300

    Wizard's page number is handled by the Router.

--- src/routes/Jarjestajat/Jarjestaja/components/JarjestajaSwitch.js
+++ src/routes/Jarjestajat/Jarjestaja/components/JarjestajaSwitch.js
@@ -2,18 +2,17 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 
 import MuutospyyntoWizardContainer from '../Hakemukset/Muutospyynto/containers/MuutospyyntoWizardContainer'
-import MuutospyyntoEditWizardContainer from '../Hakemukset/Muutospyynto/containers/MuutospyyntoEditWizardContainer'
 import JarjestajaContainer from '../containers/JarjestajaContainer'
 
-const JarjestajaSwitch = (props) => {
+const JarjestajaSwitch = () => {
 
   return (
     <Switch>
-      <Route exact path="/jarjestajat/:ytunnus/hakemukset-ja-paatokset/uusi" render={() =>{
+      <Route path="/jarjestajat/:ytunnus/hakemukset-ja-paatokset/uusi/:page" render={() =>{
         return <MuutospyyntoWizardContainer />
       } } />
-      <Route path="/jarjestajat/:ytunnus/hakemukset-ja-paatokset/:uuid" render={() =>{
-        return <MuutospyyntoEditWizardContainer />
+      <Route path="/jarjestajat/:ytunnus/hakemukset-ja-paatokset/:uuid/:page" render={() =>{
+        return <MuutospyyntoWizardContainer />
       } } />
       <Route path="/jarjestajat/:ytunnus" component={JarjestajaContainer} />
     </Switch>
