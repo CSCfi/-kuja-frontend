310ebbdb05934e7c52f1ad14cf558aa30a962e99
Raimo Pitkänen <rapitkan@gmail.com>
2019-04-11 20:59:38 +0300

    ExpandableRow component updated. It prints some debug data temporarily.

--- src/components/ExpandableRow.js
+++ src/components/ExpandableRow.js
@@ -1,66 +1,70 @@
-import React, { Component } from "react"
-import { Arrow, Heading, Span, SpanMuutos } from 'utils/UIComponents'
-import { COLORS } from "modules/styles"
-import arrow from 'static/images/koulutusala-arrow.svg'
-import PropTypes from 'prop-types'
-import ExpandableRowContent from 'components/ExpandableRowContent'
+import React from "react";
+import { Arrow, Heading, Span, SpanMuutos } from "utils/UIComponents";
+import arrow from "static/images/koulutusala-arrow.svg";
+import PropTypes from "prop-types";
+import ExpandableRowContent from "components/ExpandableRowContent";
+import { COLORS } from "modules/styles";
 
-class ExpandableRow extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            shouldBeExpanded: props.shouldBeExpanded
-        }
-        this.toggle = this.toggle.bind(this)
-    }
+class ExpandableRow extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      shouldBeExpanded: props.shouldBeExpanded
+    };
+    this.toggle = this.toggle.bind(this);
+  }
 
-    toggle = () => {
-        this.setState({
-            shouldBeExpanded: !this.state.shouldBeExpanded
-        })
-    }
+  toggle = () => {
+    this.setState({
+      shouldBeExpanded: !this.state.shouldBeExpanded
+    });
+  };
 
-    render() {
-        const muutokset = [1, 2]
-        return (
-            <div>
-                <Heading onClick={ this.toggle }>
-                    <Arrow src={ arrow } alt={ this.state.shouldBeExpanded ? 'Kutista rivi' : 'Laajenna rivi' } rotated={this.state.shouldBeExpanded} />
-                    <Span>{ this.props.number }</Span>
-                    <Span>{ this.props.title }</Span>
-                    {muutokset.length > 0 &&
-                        <SpanMuutos>
-                            Muutokset:&nbsp;
-                            <Span color={COLORS.OIVA_PURPLE}>
-                                {muutokset.length}
-                            </Span>
-                            <Span>Poista muutokset</Span>
-                        </SpanMuutos>
-                    }
-                </Heading>
-                {
-                    this.state.shouldBeExpanded &&
-                    <ExpandableRowContent
-                        code={ this.props.code }
-                        articles={ this.props.articles }
-                        content={ this.props.content }
-                        areaCode={ this.props.areaCode }
-                    >
-                    </ExpandableRowContent>
-                }
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Heading onClick={this.toggle}>
+          <Arrow
+            src={arrow}
+            alt={this.state.shouldBeExpanded ? "Kutista rivi" : "Laajenna rivi"}
+            rotated={this.state.shouldBeExpanded}
+          />
+          <Span>{this.props.code}</Span>
+          <Span>{this.props.title}</Span>
+          {this.props.changes.length ? (
+            <SpanMuutos>
+              Muutokset:&nbsp;
+              <Span color={COLORS.OIVA_PURPLE}>
+                {this.props.changes.length}
+              </Span>
+            </SpanMuutos>
+          ) : null}
+        </Heading>
+        {this.state.shouldBeExpanded && (
+          <ExpandableRowContent
+            code={this.props.code}
+            article={this.props.article}
+            changes={this.props.changes}
+            categories={this.props.categories}
+            areaCode={this.props.areaCode}
+            onFormModification={this.props.onFormModification}
+          />
+        )}
+      </div>
+    );
+  }
 }
 
 ExpandableRow.propTypes = {
-    shouldBeExpanded: PropTypes.bool,
-    title: PropTypes.string,
-    number: PropTypes.number,
-    content: PropTypes.object,
-    articles: PropTypes.array,
-    code: PropTypes.number,
-    areaCode: PropTypes.string
-}
+  areaCode: PropTypes.string,
+  article: PropTypes.object,
+  code: PropTypes.string,
+  categories: PropTypes.array,
+  changes: PropTypes.array,
+  number: PropTypes.number,
+  onFormModification: PropTypes.func,
+  shouldBeExpanded: PropTypes.bool,
+  title: PropTypes.string
+};
 
-export default ExpandableRow
+export default ExpandableRow;
