d26442cdba28cca8a57e8c2a86efcf721b5bbb2b
Meeri Panula <meeri.panula@gofore.com>
2019-04-16 12:48:42 +0300

    Muuta perusteluvalinta valintaruuduiksi

--- src/routes/Jarjestajat/Jarjestaja/Hakemukset/Muutospyynto/components/PerusteluSelect.js
+++ src/routes/Jarjestajat/Jarjestaja/Hakemukset/Muutospyynto/components/PerusteluSelect.js
@@ -1,54 +1,47 @@
-import React, { Component } from 'react'
-import 'styled-components'
-import styled from "styled-components"
-import Select from '../../../../../../modules/Select'
+import React, { Component } from "react";
+import { Field } from "redux-form";
+import styled from "styled-components";
 
-import { MUUTOS_WIZARD_TEKSTIT } from "../modules/constants"
-import { handleMuutosperusteluSelectChange } from "../modules/muutosperusteluUtil"
+import { MUUTOS_WIZARD_TEKSTIT } from "../modules/constants";
 
 const PerusteluSelectWrapper = styled.div`
   margin-bottom: 20px;
-`
-
-const SelectWrapper = styled.div`
-  width: 320px;
-`
+`;
 
 class PerusteluSelect extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      selectedOption: props.muutosperustelukoodiarvo
-    }
-  }
-
-  handleChange(selectedOption) {
-    this.setState({ selectedOption })
-    const { muutokset, fields, muutos } = this.props
-    handleMuutosperusteluSelectChange(muutokset, fields, muutos, selectedOption)
-  }
-
   render() {
-    const { selectedOption } = this.state
-    const { muutosperustelut, muutos } = this.props
-    const { koodiarvo, koodisto } = muutos
+    const { muutosperustelut, muutos, fields, muutokset } = this.props;
 
     return (
       <PerusteluSelectWrapper>
         <h4>{MUUTOS_WIZARD_TEKSTIT.TAUSTA_SYYT_OTSIKKO.FI}</h4>
         <div>{MUUTOS_WIZARD_TEKSTIT.TAUSTA_SYYT_TARKENNE.FI}</div>
-        <SelectWrapper>
-          <Select
-            name={`select-muutosperustelu-${koodisto}-${koodiarvo}`}
-            value={selectedOption}
-            options={muutosperustelut}
-            onChange={this.handleChange.bind(this)}
-            placeholder="Valitse perustelun tyyppi..."
-          />
-        </SelectWrapper>
+
+        {muutosperustelut.map(m => {
+          return (
+            <div key={m.koodiArvo}>
+              <input
+                name={`muutosperustelu-${muutos.koodiarvo}-${
+                  m.koodisto.koodistoUri
+                }-${m.koodiArvo}`}
+                id={`muutosperustelu-${muutos.koodiarvo}-${
+                  m.koodisto.koodistoUri
+                }-${m.koodiArvo}`}
+                type="checkbox"
+              />
+              <label
+                htmlFor={`muutosperustelu-${muutos.koodiarvo}-${
+                  m.koodisto.koodistoUri
+                }-${m.koodiArvo}`}
+              >
+                {m.label}
+              </label>
+            </div>
+          );
+        })}
       </PerusteluSelectWrapper>
-    )
+    );
   }
 }
 
-export default PerusteluSelect
+export default PerusteluSelect;
