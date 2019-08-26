5ee76feba49f0c085d7c3f6b00edcadbe105e129
Johannes Seppänen <johannes.seppanen@netum.fi>
2019-08-26 08:38:30 +0300

    CSCKUJA-39 added prop to CategorizedList and removed maxHeight

--- src/css/autocomplete.js
+++ src/css/autocomplete.js
@@ -13,20 +13,16 @@ export const autocompleteShortStyles = {
   }),
   valueContainer: base => ({
     ...base,
-    maxHeight: "2.4rem",
   }),
   input: base => ({
     ...base,
     margin: 0,
     padding: 0,
-    maxHeight: "2.4rem",
-    transform: `translateY(${'-15%'})`
   }),
   control: (styles) => {
     return {
       ...styles,
       backgroundColor: "white",
-      height: "2.4rem",
       minHeight: 'fit-content'
     }
   },
