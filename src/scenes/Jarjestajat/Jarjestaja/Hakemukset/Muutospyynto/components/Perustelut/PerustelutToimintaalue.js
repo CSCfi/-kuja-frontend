250856b1da34a1dba640e7644e0736860ecd5862
Raimo Pitkänen <rapitkan@gmail.com>
2019-09-02 13:36:52 +0300

    PerustelutMuut käyttää nyt FormSection:ia. Toiminta-alueen perustelut.

---
+++ src/scenes/Jarjestajat/Jarjestaja/Hakemukset/Muutospyynto/components/Perustelut/PerustelutToimintaalue.js
@@ -0,0 +1,190 @@
+import React, { useEffect, useState } from "react";
+import ExpandableRowRoot from "../../../../../../../components/02-organisms/ExpandableRowRoot";
+import FormSection from "../../../../../../../components/03-templates/FormSection";
+// import wizardMessages from "../../../../../../../i18n/definitions/wizard";
+import { isAdded, isRemoved } from "../../../../../../../css/label";
+import { injectIntl } from "react-intl";
+import PropTypes from "prop-types";
+import * as R from "ramda";
+// import { parseLocalizedField } from "../../../../../../../modules/helpers";
+
+const defaultProps = {
+  changes: [],
+  kohde: {},
+  lupa: {}
+};
+
+const PerustelutToimintaalue = React.memo(
+  ({
+    changes = defaultProps.changes,
+    handleChanges,
+    headingNumber,
+    intl,
+    kohde = defaultProps.kohde,
+    lupa = defaultProps.lupa,
+    title
+  }) => {
+    const sectionId = "perusteluttoimintaalue";
+    const [categories, setCategories] = useState([]);
+    const [groupedChanges, setGroupedChanges] = useState({});
+
+    useEffect(() => {
+      const getCategories = muutokset => {
+        return {
+          additions: R.map(muutos => {
+            return {
+              anchor: muutos.koodiarvo,
+              components: [
+                {
+                  anchor: "label",
+                  name: "StatusTextRow",
+                  properties: {
+                    labelStyles: Object.assign({}, { custom: isAdded }),
+                    title: R.find(
+                      R.propEq("kieli", R.toUpper(intl.locale)),
+                      muutos.koodi.metadata
+                    ).nimi
+                  }
+                }
+              ],
+              categories: [
+                {
+                  anchor: "tekstikentta",
+                  components: [
+                    {
+                      anchor: "A",
+                      name: "TextBox",
+                      properties: {
+                        placeholder: "Sana on vapaa..."
+                      }
+                    }
+                  ]
+                }
+              ]
+            };
+          }, muutokset.LISAYS),
+          removals: R.map(muutos => {
+            return {
+              anchor: muutos.koodiarvo,
+              components: [
+                {
+                  anchor: "label",
+                  name: "StatusTextRow",
+                  properties: {
+                    labelStyles: Object.assign({}, { custom: isRemoved }),
+                    title: R.find(
+                      R.propEq("kieli", R.toUpper(intl.locale)),
+                      muutos.koodi.metadata
+                    ).nimi
+                  }
+                }
+              ],
+              categories: [
+                {
+                  anchor: "tekstikentta",
+                  components: [
+                    {
+                      anchor: "A",
+                      name: "TextBox",
+                      properties: {
+                        placeholder: "Sana on vapaa..."
+                      }
+                    }
+                  ]
+                }
+              ]
+            };
+          }, muutokset.POISTO)
+        };
+      };
+      if (
+        R.compose(
+          R.not,
+          R.isEmpty
+        )(changes)
+      ) {
+        const categories = getCategories(R.groupBy(R.prop("tila"), changes));
+        setCategories(categories);
+      }
+    }, [changes, intl]);
+
+    useEffect(() => {
+      const tmpGroupedChanges = R.groupBy(R.prop("tila"), changes);
+      const additions = R.map(muutos => {
+        return {
+          anchor: `additions.${muutos.koodiarvo}.label`,
+          properties: {
+            a: 1
+          }
+        };
+      }, tmpGroupedChanges.LISAYS);
+      const removals = R.map(muutos => {
+        return {
+          anchor: `removals.${muutos.koodiarvo}.label`,
+          properties: {
+            b: 1
+          }
+        };
+      }, tmpGroupedChanges.POISTO);
+      console.info(additions, removals);
+      setGroupedChanges({ additions, removals });
+    }, [changes]);
+
+    return (
+      <React.Fragment>
+        {R.compose(
+          R.not,
+          R.isEmpty
+        )(groupedChanges) ? (
+          <FormSection
+            id={sectionId}
+            sectionChanges={groupedChanges}
+            code={headingNumber}
+            title={title}
+            runOnChanges={handleChanges}
+            render={props => (
+              <React.Fragment>
+                <ExpandableRowRoot
+                  anchor="additions"
+                  key={`perustelut-toimintaalue-lisatyt`}
+                  categories={categories.additions}
+                  changes={props.sectionChanges.additions}
+                  disableReverting={true}
+                  hideAmountOfChanges={false}
+                  showCategoryTitles={true}
+                  isExpanded={true}
+                  sectionId={sectionId}
+                  title="Lisätyt"
+                  {...props}
+                />
+                <ExpandableRowRoot
+                  anchor="removals"
+                  key={`perustelut-toimintaalue-poistetut`}
+                  categories={categories.removals}
+                  changes={props.sectionChanges.removals}
+                  disableReverting={true}
+                  hideAmountOfChanges={false}
+                  showCategoryTitles={true}
+                  isExpanded={true}
+                  sectionId={sectionId}
+                  title="Poistetut"
+                  {...props}
+                />
+              </React.Fragment>
+            )}
+          />
+        ) : null}
+      </React.Fragment>
+    );
+  }
+);
+
+PerustelutToimintaalue.propTypes = {
+  changes: PropTypes.array,
+  handleChanges: PropTypes.func,
+  headingNumber: PropTypes.number,
+  kohde: PropTypes.object,
+  lupa: PropTypes.object
+};
+
+export default injectIntl(PerustelutToimintaalue);
