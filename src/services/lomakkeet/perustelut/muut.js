57397198a9eef941f20aa03d4878764e7aef75be
Johannes Seppänen <johannes.seppanen@netum.fi>
2019-09-13 18:59:17 +0300

    CSCOIVA-935 added tyovoima form to muut.js

--- src/services/lomakkeet/perustelut/muut.js
+++ src/services/lomakkeet/perustelut/muut.js
@@ -243,7 +243,176 @@ export const getVankilaopetusPerustelulomake = () => {
     }
   ];
 };
+export const getTyovoimakoulutuslomake = () => {
+  const code = 4;
+  return [
+    {
+      anchor: `${code}-1`,
+      title: "Työvoimakoulutuksen tehtävä",
+      components: [
+        {
+          anchor: "tehtavan-tarpeellisuus-label",
+          name: "StatusTextRow",
+          properties: {
+            code: 1,
+            labelStyles: {},
+            title: "Tehtävän tarpeellisuus"
+          }
+        }
+      ],
+      categories: [
+        {
+          anchor: "tehtavan-tarpeellisuus-field",
+          components: [
+            {
+              anchor: "A",
+              name: "TextBox",
+              properties: {
+                placeholder:
+                  "Perustelkaa tehtävän tarpeellisuus ensisijaisella toiminta-alueellanne"
+              }
+            }
+          ]
+        }
+      ]
+    },
+    {
+      anchor: `${code}-2`,
+      title: "",
+      components: [
+        {
+          anchor: "toiminnalliset-edellytykset-label",
+          name: "StatusTextRow",
+          properties: {
+            code: 2,
+            labelStyles: {},
+            title: "Toiminnalliset edellytykset laajennetun oppisopimus järjestämistehtävän järjestämiseksi"
+          }
+        }
+      ],
+      categories: [
+        {
+          anchor: "henkilostoresurssit-field",
+          components: [
+            {
+              anchor: "A",
+              name: "TextBox",
+              properties: {
+                placeholder:
+                  "Haetun tehtävän edellyttämät henkilöstöresurssit (mm. tehtävän edellyttämä henkilöstön määrä, sekä rekrytoitavien määrä). Huom. Henkilötietoja ei tule antaa lomakkeella."
+              }
+            }
+          ]
+        },
+        {
+          anchor: "osaaminen-field",
+          components: [
+            {
+              anchor: "A",
+              name: "TextBox",
+              properties: {
+                placeholder:
+                  "Haetun tehtävän järjestämisen edellyttämä osaaminen (mm. palveluprosessi, hankintaosaaminen, johtaminen ja sopimusten valvonta)."
+              }
+            }
+          ]
+        },
+        {
+          anchor: "pedagogisetjarjestelyt-field",
+          components: [
+            {
+              anchor: "A",
+              name: "TextBox",
+              properties: {
+                placeholder:
+                  "Haetun tehtävän edellyttämä työelämäpalvelu ja muu sidosryhmäyhteistyö (mm. toimijat, toiminta- ja yhteistyömuodot)."
+              }
+            }
+          ]
+        },
+        {
+          anchor: "sidosryhmayhteistyo-field",
+          components: [
+            {
+              anchor: "A",
+              name: "TextBox",
+              properties: {
+                placeholder:
+                  "Haetun tehtävän edellyttämät toimivat ja kokonaisvaltaiset työelämäyhteydet ja -palvelut ja muu sidosryhmäyhteistyö (mm. toimijat, toiminta- ja yhteistyömuodot)."
+              }
+            }
+          ]
+        }
+      ]
+    },
+    {
+      anchor: `${code}-3`,
+      title: "",
+      components: [
+        {
+          anchor: "suunnitelma-label",
+          name: "StatusTextRow",
+          properties: {
+            code: 3,
+            labelStyles: {},
+            title: " Suunnitelma työvoimakoulutuksen järjestämiseksi"
+          }
+        }
+      ],
+      categories: [
+        {
+          anchor: "suunnitelma-field",
+          components: [
+            {
+              anchor: "A",
+              name: "TextBox",
+              properties: {
+                placeholder:
+                  "Toimintamalli työvoimakoulutuksen suunnittelusta ja toteutuksesta (mm. koulutusalat, joille työvoimakoulutusta aiotaan järjestää)."
+              }
+            }
+          ]
+        }
+      ]
+    },
+    {
+      anchor: `${code}-4`,
+      title: "",
+      components: [
+        {
+          anchor: "ely-label",
+          name: "StatusTextRow",
+          properties: {
+            code: 4,
+            labelStyles: {},
+            title: "ELY-keskusten kanssa suunniteltu yhteistyö"
+          }
+        }
+      ],
+      categories: [
+        {
+          anchor: "ely-autocomplete",
+          components: [
+            {
+              anchor: "A",
+              name: "Autocomplete",
+              properties: {
+                options: [
+                  {label: "A", value: "A"},
+                  {label: "B", value: "B"},
+                  {label: "C", value: "C"}
+                ]
 
+              }
+            }
+          ]
+        }
+      ]
+    }
+  ]
+
+
+}
 const opiskelijavuodet = (code, anchorNumber) => {
   const year = new Date().getFullYear();
   return {
