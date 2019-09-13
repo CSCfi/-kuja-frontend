38f5c3c2a8178145c4d8b3668f93884a8309864a
Johannes Seppänen <johannes.seppanen@netum.fi>
2019-09-13 19:16:40 +0300

    CSCOIVA-936 oppisopimus perustelut form

---
+++ src/components/04-forms/muut/OppisopimusPerusteluLomake/oppisopimusPerusteluLomake.stories.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { withInfo } from "@storybook/addon-info";
+import OppisopimusPerusteluLomake from "./index";
+
+storiesOf("Lomakkeet/Muut", module)
+  .addDecorator(withInfo)
+  .add("OppisopimusPerusteluLomake", () => {
+    const changes = [
+
+    ];
+    return (
+      <OppisopimusPerusteluLomake
+        changes={changes}
+      ></OppisopimusPerusteluLomake>
+    );
+  });
--- src/services/lomakkeet/perustelut/muut.js
+++ src/services/lomakkeet/perustelut/muut.js
@@ -244,6 +244,132 @@ export const getVankilaopetusPerustelulomake = () => {
   ];
 };
 
+export const getOppisopimusPerusteluLomake = () => {
+  const code = 4;
+  return [
+    {
+      anchor: `${code}-1`,
+      title: "Laajennettu oppisopimuskoulutuksen järjestämistehtävä",
+      components: [
+        {
+          anchor: "tehtavan-tarpeellisuus-label",
+          name: "StatusTextRow",
+          properties: {
+            code: 1,
+            labelStyles: {},
+            title: "Tehtävän tarpeellisuus"
+          }
+        }
+      ],
+      categories: [
+        {
+          anchor: "tehtavan-tarpeellisuus-field",
+          components: [
+            {
+              anchor: "A",
+              name: "TextBox",
+              properties: {
+                placeholder:
+                  "Perustelkaa tehtävän tarpeellisuus ensisijaisella toiminta-alueellanne"
+              }
+            }
+          ]
+        }
+      ]
+    },
+    {
+      anchor: `${code}-2`,
+      title: "",
+      components: [
+        {
+          anchor: "toiminnalliset-edellytykset-label",
+          name: "StatusTextRow",
+          properties: {
+            code: 2,
+            labelStyles: {},
+            title: "Toiminnalliset edellytykset laajennetun oppisopimus järjestämistehtävän järjestämiseksi"
+          }
+        }
+      ],
+      categories: [
+        {
+          anchor: "henkilostoresurssit-field",
+          components: [
+            {
+              anchor: "A",
+              name: "TextBox",
+              properties: {
+                placeholder:
+                  "Haetun tehtävän edellyttämät henkilöstöresurssit (mm. tehtävän edellyttämä henkilöstön määrä, sekä rekrytoitavien määrä). Huom. Henkilötietoja ei tule antaa lomakkeella."
+              }
+            }
+          ]
+        },
+        {
+          anchor: "osaaminen-field",
+          components: [
+            {
+              anchor: "A",
+              name: "TextBox",
+              properties: {
+                placeholder:
+                  "Haetun tehtävän järjestämisen edellyttämä osaaminen (mm. palveluprosessi, hankintaosaaminen, johtaminen ja sopimusten valvonta)."
+              }
+            }
+          ]
+        },
+        {
+          anchor: "sidosryhmayhteistyo-field",
+          components: [
+            {
+              anchor: "A",
+              name: "TextBox",
+              properties: {
+                placeholder:
+                  "Haetun tehtävän edellyttämät toimivat ja kokonaisvaltaiset työelämäyhteydet ja -palvelut ja muu sidosryhmäyhteistyö (mm. toimijat, toiminta- ja yhteistyömuodot)."
+              }
+            }
+          ]
+        }
+      ]
+    },
+    {
+      anchor: `${code}-3`,
+      title: "",
+      components: [
+        {
+          anchor: "arvio-label",
+          name: "StatusTextRow",
+          properties: {
+            code: 3,
+            labelStyles: {},
+            title: "Arvio koulutukseen suunnattavista opiskelijavuosista seuraavana kolmena vuotena"
+          }
+        }
+      ],
+      categories: [
+        {
+          anchor: "arvio-opiskelijavuodet-label",
+          name: "StatusTextRow",
+          properties: {
+            labelStyles: {},
+            title: "Merkitkää arvionne tehtävään kohdistettavista opiskelijavuosista po. vuosina.",
+            withoutMargin: true
+          }
+        },
+        {
+          anchor: "arvio-opiskelijavuodet-input-field",
+          name: "Input",
+          properties: {
+            type: 'number'
+          }
+        },
+
+      ]
+    }
+  ]
+}
+
 const opiskelijavuodet = (code, anchorNumber) => {
   const year = new Date().getFullYear();
   return {
