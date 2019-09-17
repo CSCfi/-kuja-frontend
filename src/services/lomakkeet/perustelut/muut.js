3fe2f666743927c26ee1ed195ea1ae6fcd3dc0e7
Johannes Seppänen <johannes.seppanen@netum.fi>
2019-09-17 10:14:55 +0300

    CSCOIVA-938 implementation

--- src/services/lomakkeet/perustelut/muut.js
+++ src/services/lomakkeet/perustelut/muut.js
@@ -244,6 +244,257 @@ export const getVankilaopetusPerustelulomake = () => {
   ];
 };
 
+export const getVaativaErityinenTukilomake = () => {
+  const code = 4;
+  return [{
+    anchor: "vaativa-erityinen-tuki-perustelut-lomake",
+    styleClasses:
+      ["px-10 py-10"],
+    components:
+      [
+        {
+          anchor: "vaativa-erityinen-tuki-perustelut-title",
+          styleClasses: ["text-base"],
+          name: "StatusTextRow",
+          properties: {
+            title: "Vaativan erityisen tuen tehtävä"
+          }
+        }
+      ],
+    categories:
+      [
+        {
+          anchor: `${code}-0`,
+          styleClasses: ["border-t px-4 py-8 hover:bg-gray-100"],
+          components: [
+            {
+              anchor: "koulutukset-label",
+              styleClasses: ["text-base"],
+              name: "StatusTextRow",
+              properties: {
+                labelStyles: {},
+                title: "Vaativan erityisen tuen tehtävää on haettu seuraaviin koulutuksiin:"
+              }
+            }
+          ],
+          categories: [
+            {
+              anchor: "tutkintokoulutukseen-label",
+              styleClasses: ["pl-6"],
+              title: 'Tutkintokoulutukseen',
+              categories: [
+                {
+                  anchor: "tutkintokoulutus-label",
+                  styleClasses: ["pl-6"],
+                  components: [
+                    {
+                      anchor: "A",
+                      styleClasses: ["text-base"],
+                      name: "StatusTextRow",
+                      properties: {
+                        labelStyles: {},
+                      }
+                    }
+                  ]
+                }
+              ]
+            },
+            {
+              anchor: "tutkintokoulutukseen-label",
+              styleClasses: ["pl-6"],
+              title: 'Ammatilliseen koulutukseen valmentavaan koulutukseen',
+              categories: [
+                {
+                  anchor: "tutkintokoulutus-label",
+                  styleClasses: ["pl-6"],
+                  components: [
+                    {
+                      anchor: "A",
+                      styleClasses: ["text-base"],
+                      name: "StatusTextRow",
+                      properties: {
+                        labelStyles: {},
+                      }
+                    }
+                  ]
+                }
+              ]
+            },
+            {
+              anchor: "tutkintokoulutukseen-label",
+              styleClasses: ["pl-6"],
+              title: 'Työhön ja itsenäiseen elämään valmentavaan koulutukseen',
+              categories: [
+                {
+                  anchor: "tutkintokoulutus-label",
+                  styleClasses: ["pl-6"],
+                  components: [
+                    {
+                      anchor: "A",
+                      styleClasses: ["text-base"],
+                      name: "StatusTextRow",
+                      properties: {
+                        labelStyles: {},
+                      }
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        },
+        {
+          anchor: `${code}-1`,
+          styleClasses: ["border-t px-4 py-8 hover:bg-gray-100"],
+          components: [
+            {
+              anchor: "tehtavan-tarpeellisuus-label",
+              styleClasses: ["text-base"],
+              name: "StatusTextRow",
+              properties: {
+                code: 1,
+                labelStyles: {},
+                title: "Tehtävän tarpeellisuus"
+              }
+            }
+          ],
+          categories: [
+            {
+              anchor: "tehtavan-tarpeellisuus-field",
+              styleClasses: ["pl-6"],
+              components: [
+                {
+                  anchor: "A",
+                  name: "TextBox",
+                  properties: {
+                    placeholder:
+                      "Perustelkaa tehtävän tarpeellisuus ensisijaisella toiminta-alueellanne"
+                  }
+                }
+              ]
+            }
+          ]
+        },
+        {
+          anchor: `${code}-2`,
+          styleClasses: ["border-t px-4 py-8 hover:bg-gray-100"],
+          components: [
+            {
+              anchor: "toiminnalliset-edellytykset-label",
+              name: "StatusTextRow",
+              styleClasses: ["text-base"],
+              properties: {
+                code: 2,
+                labelStyles: {},
+                title: "Toiminnalliset edellytykset laajennetun oppisopimus järjestämistehtävän järjestämiseksi"
+              }
+            }
+          ],
+          categories: [
+            {
+              anchor: "henkilostoresurssit-field",
+              title: 'Henkilöstöresurssit',
+              styleClasses: ["pl-6"],
+              components: [
+                {
+                  anchor: "A",
+                  name: "TextBox",
+                  properties: {
+                    placeholder:
+                      "Haetun tehtävän edellyttämät henkilöstöresurssit (mm. tehtävän edellyttämä henkilöstön määrä, sekä rekrytoitavien määrä). Huom. Henkilötietoja ei tule antaa lomakkeella."
+                  }
+                }
+              ]
+            },
+            {
+              anchor: "osaaminen-field",
+              title: 'Osaaminen',
+              styleClasses: ["pl-6"],
+              components: [
+                {
+                  anchor: "A",
+                  name: "TextBox",
+                  properties: {
+                    placeholder:
+                      "Haetun tehtävän järjestämisen edellyttämä osaaminen (mm. palveluprosessi, hankintaosaaminen, johtaminen ja sopimusten valvonta)."
+                  }
+                }
+              ]
+            },
+            {
+              anchor: "jarjestelyt-field",
+              title: 'Pedagogiset järjestelyt',
+              styleClasses: ["pl-6"],
+              components: [
+                {
+                  anchor: "A",
+                  name: "TextBox",
+                  properties: {
+                    placeholder:
+                      "Haetun tehtävän edellyttämät pedagogiset järjestelyt (mm. opetusjärjestelyt, oppimisympäristöt, tilat ja välineet)."
+                  }
+                }
+              ]
+            },
+            {
+              anchor: "sidosryhmayhteistyo-field",
+              title: 'Sidosryhmäyhteistyö',
+              styleClasses: ["pl-6"],
+              components: [
+                {
+                  anchor: "A",
+                  name: "TextBox",
+                  properties: {
+                    placeholder:
+                      "Haetun tehtävän edellyttämät toimivat ja kokonaisvaltaiset työelämäyhteydet ja -palvelut ja muu sidosryhmäyhteistyö (mm. toimijat, toiminta- ja yhteistyömuodot)."
+                  }
+                }
+              ]
+            }
+          ]
+        },
+        {
+          anchor: `${code}-3`,
+          styleClasses: ["border-t px-4 py-8 hover:bg-gray-100"],
+          components: [
+            {
+              anchor: "suunnitelma-label",
+              name: "StatusTextRow",
+              styleClasses: ["text-base"],
+              properties: {
+                code: 3,
+                labelStyles: {},
+                title: " Suunnitelma työvoimakoulutuksen järjestämiseksi"
+              }
+            }
+          ],
+          categories: [
+            {
+              anchor: "suunnitelma-field",
+              styleClasses: ["pl-6"],
+              components: [
+                {
+                  anchor: "A",
+                  name: "TextBox",
+                  properties: {
+                    placeholder:
+                      "Toimintamalli työvoimakoulutuksen suunnittelusta ja toteutuksesta (mm. koulutusalat, joille työvoimakoulutusta aiotaan järjestää)."
+                  }
+                }
+              ]
+            }
+          ]
+        },
+        {
+          anchor: `${code}-5`,
+          styleClasses: ["border-t px-4 py-8 hover:bg-gray-100"],
+          categories: [opiskelijavuodet(4, 4)]
+        }
+      ]
+  }]
+}
+
+
 export const getOppisopimusPerusteluLomake = () => {
   const code = 4;
   return [
