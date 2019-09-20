34b48641f0584f3358987fe509f929a32da8060b
Sami Heikkinen <sami.heikkinen@valamis.com>
2019-09-20 11:25:13 +0300

    Yleiset section change handling

--- src/scenes/Jarjestajat/Jarjestaja/Hakemukset/Muutospyynto/components/MuutospyyntoWizardTaloudelliset.js
+++ src/scenes/Jarjestajat/Jarjestaja/Hakemukset/Muutospyynto/components/MuutospyyntoWizardTaloudelliset.js
@@ -1,11 +1,11 @@
-import _ from "lodash";
-import React, { Component } from "react";
+import React from "react";
 import { MUUTOS_WIZARD_TEKSTIT } from "../modules/constants";
 import FormSection from "../../../../../../components/03-templates/FormSection";
 import { injectIntl } from "react-intl";
 import TaloudellisetYleisettiedot from "./Taloudelliset/TaloudellisetYleisettiedot";
 import TaloudellisetInvestoinnit from "./Taloudelliset/TaloudellisetInvestoinnit";
 import PropTypes from "prop-types";
+import * as R from "ramda";
 
 const MuutospyyntoWizardTaloudelliset = ({
   changeObjects,
@@ -22,45 +22,8 @@ const MuutospyyntoWizardTaloudelliset = ({
   onChangesUpdate,
   onStateUpdate
 }) => {
-  // let { taloudellisetValue } = this.props;
-  // const tutkintojaLisatty = _.find(tutkinnotjakoulutuksetValue, function(t) {
-  //   return t.type === MUUTOS_TYPES.ADDITION;
-  // });
-
-  // if (initialValues) console.log(initialValues);
-  // if (!taloudellisetValue) {
-  //   if (
-  //     initialValues &&
-  //     initialValues.taloudelliset &&
-  //     initialValues.taloudelliset.taloudelliset
-  //   ) {
-  //     taloudellisetValue = initialValues.taloudelliset.taloudelliset[0];
-  //   } else taloudellisetValue = taloudellisetValue;
-
-  //   if (!taloudellisetValue || taloudellisetValue.length === 0) {
-  //     taloudellisetValue = [
-  //       {
-  //         edellytykset: null,
-  //         vaikutukset: null,
-  //         sopeuttaminen: null,
-  //         investoinnit: null,
-  //         kustannukset: null,
-  //         rahoitus: null,
-  //         omavaraisuusaste: null,
-  //         maksuvalmius: null,
-  //         velkaantuneisuus: null,
-  //         kannattavuus: null,
-  //         kumulatiivinen: null
-  //       }
-  //     ];
-  //   }
-  //   console.log(taloudellisetValue);
-  // }
-
   return (
-    <FormSection
-    // onSubmit={handleSubmit}
-    >
+    <FormSection>
       <h2 className="mb-4">
         {MUUTOS_WIZARD_TEKSTIT.TALOUDELLISET.PAAOTSIKKO.FI}
       </h2>
@@ -70,49 +33,41 @@ const MuutospyyntoWizardTaloudelliset = ({
       )}
       {changeObjects && (
         <React.Fragment>
-          <TaloudellisetYleisettiedot
+          <FormSection
+            id="tutkinnot"
+            render={_props => (
+              <React.Fragment>
+                <TaloudellisetYleisettiedot
+                  stateObject={R.path(
+                    ["taloudelliset", "yleisettiedot"],
+                    muutoshakemus
+                  )}
+                  changeObjects={{
+                    tutkinnot: R.path(["tutkinnot"], changeObjects),
+                    taloudelliset: R.path(
+                      ["taloudelliset", "yleisettiedot"],
+                      changeObjects
+                    )
+                  }}
+                  {..._props}
+                />
+              </React.Fragment>
+            )}
+            runOnStateUpdate={onStateUpdate}
+            runOnChanges={onChangesUpdate}
+          />
+
+          {/* <TaloudellisetInvestoinnit
             stateObject={{}}
             onStateUpdate={onStateUpdate}
             onChangesUpdate={onChangesUpdate}
             changeObjects={changeObjects}
-          />
-          <TaloudellisetInvestoinnit
-            stateObject={{}}
-            onStateUpdate={onStateUpdate}
-            onChangesUpdate={onChangesUpdate}
-            changeObjects={changeObjects}
-          />
+          /> */}
         </React.Fragment>
       )}
     </FormSection>
   );
 };
-
-// const selector = formValueSelector(FORM_NAME_UUSI_HAKEMUS)
-
-// MuutospyyntoWizardTaloudelliset = connect(state => {
-//   const tutkinnotjakoulutuksetValue = selector(state, FIELD_ARRAY_NAMES.TUTKINNOT_JA_KOULUTUKSET)
-//   // const taloudellisetValue = selector(state, FIELD_ARRAY_NAMES.TALOUDELLISET)
-//   let formVals = undefined
-//   let taloudellisetValue = undefined
-//   if (state.form && state.form.uusiHakemus && state.form.uusiHakemus.values) {
-//     formVals = state.form.uusiHakemus.values;
-//     if (state.form.uusiHakemus.values.taloudelliset && state.form.uusiHakemus.values.taloudelliset.length>0) taloudellisetValue = state.form.uusiHakemus.values.taloudelliset[0];
-//   }
-
-//   return {
-//     tutkinnotjakoulutuksetValue,
-//     taloudellisetValue,
-//     formValues: formVals
-//   }
-// })(MuutospyyntoWizardTaloudelliset)
-
-// export default reduxForm({
-//   form: FORM_NAME_UUSI_HAKEMUS,
-//   destroyOnUnmount: false,
-//   forceUnregisterOnUnmount: true,
-//   validate
-// })(MuutospyyntoWizardTaloudelliset)
 
 MuutospyyntoWizardTaloudelliset.propTypes = {
   changeObjects: PropTypes.object,
--- src/scenes/Jarjestajat/Jarjestaja/Hakemukset/Muutospyynto/components/Taloudelliset/TaloudellisetYleisettiedot.js
+++ src/scenes/Jarjestajat/Jarjestaja/Hakemukset/Muutospyynto/components/Taloudelliset/TaloudellisetYleisettiedot.js
@@ -1,115 +1,106 @@
-import React, { useEffect, useState, useMemo } from "react";
+import React, { useEffect, useMemo } from "react";
 import ExpandableRowRoot from "../../../../../../../components/02-organisms/ExpandableRowRoot";
 import { injectIntl } from "react-intl";
 import PropTypes from "prop-types";
-import _ from "lodash";
+import * as R from "ramda";
 
 const TaloudellisetYleisettiedot = React.memo(props => {
   const { onStateUpdate } = props;
-  const sectionId = "taloudellisetyleisettiedot";
-  const [cats, setCats] = useState([]);
+  const sectionId = "taloudelliset_yleisettiedot";
 
   const getCategories = useMemo(() => {
     return () => {
       let structure = null;
 
-      structure = {
-        anchor: sectionId,
-        components: [
-          {
-            anchor: "A",
-            name: "taloudellisetyleisettiedot"
-          }
-        ],
-        categories: [
-          {
-            anchor: "edellytykset-tekstikentta",
-            title: "Taloudelliset edellytykset",
-            styleClasses: ["mb-6"],
-            components: [
-              {
-                anchor: "A",
-                name: "TextBox",
-                properties: {
-                  placeholder: ""
+      structure = [
+        {
+          anchor: "yleisettiedot",
+          categories: [
+            {
+              anchor: "edellytykset-tekstikentta",
+              title: "Taloudelliset edellytykset",
+              styleClasses: ["mb-6"],
+              components: [
+                {
+                  anchor: "A",
+                  name: "TextBox",
+                  properties: {
+                    placeholder: ""
+                  }
                 }
-              }
-            ]
-          },
-          {
-            anchor: "Vaikutukset-tekstikentta",
-            title: "Vaikutukset taloudellisten resurssien kohdentamiseen",
-            styleClasses: ["mb-6"],
-            components: [
-              {
-                anchor: "B",
-                name: "TextBox",
-                properties: {
-                  placeholder: ""
+              ]
+            },
+            {
+              anchor: "Vaikutukset-tekstikentta",
+              title: "Vaikutukset taloudellisten resurssien kohdentamiseen",
+              styleClasses: ["mb-6"],
+              components: [
+                {
+                  anchor: "B",
+                  name: "TextBox",
+                  properties: {
+                    placeholder: ""
+                  }
                 }
-              }
-            ]
-          },
-          {
-            anchor: "sopeuttaminen-tekstikentta",
-            title: "Toiminnan ja talouden sopeuttaminen",
-            styleClasses: [""],
-            components: [
-              {
-                anchor: "C",
-                name: "TextBox",
-                properties: {
-                  placeholder: ""
+              ]
+            },
+            {
+              anchor: "sopeuttaminen-tekstikentta",
+              title: "Toiminnan ja talouden sopeuttaminen",
+              styleClasses: [""],
+              components: [
+                {
+                  anchor: "C",
+                  name: "TextBox",
+                  properties: {
+                    placeholder: ""
+                  }
                 }
-              }
-            ]
-          }
-        ]
-      };
+              ]
+            }
+          ]
+        }
+      ];
       return structure;
     };
-  }, [props.changeObjects]);
+  }, []);
 
   useEffect(() => {
-    // for testing only
-    setCats(
+    const array = getCategories();
+    console.log(array);
+
+    onStateUpdate(
       {
-        categories: getCategories()
+        categories: array
       },
       sectionId
     );
-
-    onStateUpdate([
-      {
-        categories: getCategories()
-      },
-      sectionId
-    ]);
-  }, [getCategories]);
-  console.log(cats);
+  }, [getCategories, onStateUpdate]);
   return (
     <React.Fragment>
-      <ExpandableRowRoot
-        id={sectionId}
-        title="Yleiset tiedot"
-        anchor="yleiset"
-        key={`taloudelliset-yleisetiedot`}
-        // categories={props.stateObject.categories}
-        categories={cats}
-        disableReverting={true}
-        hideAmountOfChanges={false}
-        showCategoryTitles={true}
-        isExpanded={true}
-        sectionId={sectionId}
-        onUpdate={props.onChangesUpdate}
-        {...props}
-      />
+      <p></p>
+      {!!R.path(["categories"], props.stateObject) && (
+        <ExpandableRowRoot
+          title={"Yleiset tiedot"}
+          anchor={sectionId}
+          key={`taloudelliset-yleisetiedot`}
+          categories={props.stateObject.categories}
+          changes={R.path(["taloudelliset"], props.changeObjects)}
+          disableReverting={true}
+          hideAmountOfChanges={false}
+          showCategoryTitles={true}
+          isExpanded={true}
+          sectionId={sectionId}
+          onUpdate={props.onChangesUpdate}
+          {...props}
+        />
+      )}
     </React.Fragment>
   );
 });
 
 TaloudellisetYleisettiedot.propTypes = {
-  changes: PropTypes.array,
+  changeObjects: PropTypes.object,
   handleChanges: PropTypes.func,
   headingNumber: PropTypes.number,
   kohde: PropTypes.object,
