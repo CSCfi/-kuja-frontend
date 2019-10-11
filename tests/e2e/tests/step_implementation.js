4af7d0fe6d21905bc44c17f3668204d4a6a77dd4
Raimo Pitkänen <rapitkan@gmail.com>
2019-10-11 17:16:16 +0300

    Lisää e2e-testejä.

--- tests/e2e/tests/step_implementation.js
+++ tests/e2e/tests/step_implementation.js
@@ -1,18 +1,23 @@
 /* globals gauge*/
 "use strict";
 const {
-  goto,
-  link,
-  click,
-  openBrowser,
-  closeBrowser,
-  write,
-  inputField,
-  focus,
+  $,
   button,
-  text
+  goto,
+  click,
+  closeBrowser,
+  currentURL,
+  focus,
+  inputField,
+  link,
+  openBrowser,
+  scrollDown,
+  scrollTo,
+  text,
+  write
 } = require("taiko");
-const assert = require("assert");
+const chai = require("chai");
+const assert = chai.assert;
 const headless = process.env.headless_chrome.toLowerCase() === "true";
 
 beforeSuite(async () => {
@@ -25,17 +30,19 @@ afterSuite(async () => {
 
 step("Navigate to app", async () => {
   try {
-    await goto("https://oivadev.csc.fi/");
+    // await goto("https://oivadev.csc.fi/");
+    await goto("http://localhost");
   } catch (e) {
-    console.error(e);
+    await click("Lisäasetukset");
+    await click("Siirry sivustoon localhost (tämä ei ole turvallista)");
   }
 });
 
-step("Log in", async () => {
+step("Log in as <username>", async username => {
   await click(link({ href: "/cas-auth" }));
-  await write("oiva-sanni");
+  await write(username);
   await focus(inputField({ type: "password" }));
-  await write("oiva-sanni"); 
+  await write("oiva-sanni");
   await click(button({ type: "submit" }));
   assert.ok(await text("Kirjaudu ulos").exists());
 });
@@ -52,6 +59,59 @@ step("Jarjestamislupa", async () => {
   try {
     click("Järjestämislupa");
     assert.ok(await text("Tutkinnot ja koulutukset").exists());
+  } catch (e) {
+    console.error(e);
+  }
+});
+
+step("Avaa uusi muutospyyntolomake", async () => {
+  try {
+    await click(link("Järjestämislupa-asiat"));
+    await click(link("Uusi hakemus"));
+  } catch (e) {
+    console.error(e);
+  }
+});
+
+step("Edellinen sivu", async () => {
+  try {
+    await click($("button.previous"));
+  } catch (e) {
+    console.error(e);
+  }
+});
+
+step("Seuraava sivu", async () => {
+  try {
+    await click($("button.next"));
+  } catch (e) {
+    console.error(e);
+  }
+});
+
+step("Tarkista, että ollaan sivulla <pageNumber>", async pageNumber => {
+  try {
+    assert.include(await currentURL(), `uusi/${pageNumber}`);
+  } catch (e) {
+    console.error(e);
+  }
+});
+
+step("Lomakeoperaatio <sectionId> valitse <item>", async (sectionId, item) => {
+  try {
+    await scrollDown($(".MuiDialogContent-root"));
+    await scrollTo(item);
+    await click(item);
+  } catch (e) {
+    console.error(e);
+  }
+});
+
+step("Sulje lomake", async () => {
+  try {
+    await click($('button[aria-label="Close"]'));
+    await click("Kyllä");
+    assert.notInclude(await currentURL(), "hakemukset-ja-paatokset");
   } catch (e) {
     console.error(e);
   }
