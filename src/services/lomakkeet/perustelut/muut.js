d0c57ee7d31c8e4f0b82f2a949ea1df7172b9002
JP Yliuntinen <juho-petteri.yliuntinen@valamis.com>
2019-10-27 14:53:12 +0200

    Fix the accidental ä in an anchor name

--- src/services/lomakkeet/perustelut/muut.js
+++ src/services/lomakkeet/perustelut/muut.js
@@ -693,7 +693,7 @@ export const getOpiskelijavuodetVaativaKoulutusPerustelulomake = (isReadOnly = f
 export const getOpiskelijavuodetSisaoppilaitosmuotoinenOpetusPerustelulomake = (isReadOnly = false, changeObject, differenceComponentTitles) => {
   return [
     {
-      anchor: "sisäoppilaitos",
+      anchor: "sisaoppilaitos",
       title: "Haettava määrä",
       components: [
         generateDifferenceComponent({changeObject, titles: differenceComponentTitles, isReadOnly: true })
