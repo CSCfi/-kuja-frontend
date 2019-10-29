97be504162ed1757586769fc82c5f8c511f4ca1d
JP Yliuntinen <juho-petteri.yliuntinen@valamis.com>
2019-10-29 12:02:20 +0200

    Add functions for fetching single anchored elements from category/component hierarchies

--- src/utils/common.js
+++ src/utils/common.js
@@ -7,6 +7,52 @@ export const getAnchorPart = (anchor, index) => {
   )(anchor || "");
 };
 
+/**
+ * Returns the element with the given anchor from the array of elements
+ * @param anchor
+ * @param scanArray
+ */
+export const findAnchoredElement = (anchor, scanArray) => {
+  return R.find(R.propEq("anchor", anchor), scanArray || []);
+};
+
+export const findAnchoredCategoryFromElement = (anchor, elementObject) => {
+  return findAnchoredElement(anchor, elementObject.categories);
+};
+
+export const findAnchoredComponentFromElement = (anchor, elementObject) => {
+  return findAnchoredElement(anchor, elementObject.components);
+};
+
+const findAnchoredCategoryOrComponentFromElement = (anchor, elementObject) => {
+  let retval = findAnchoredCategoryFromElement(anchor, elementObject);
+  if(!retval) retval = findAnchoredComponentFromElement(anchor, elementObject);
+  return retval;
+}
+
+/**
+ * Returns the form component object found from given anchor in a state object. We expect that the anchor is
+ * a . delimited path with starting elements being category anchors and the last element being a component anchor
+ *
+ * @param anchor The path for scanning the component from stateObject (e.g. vahimmaisopiskelijavuodet.A)
+ * @param stateObject
+ */
+export const findComponentFromCategoryHierarchyWithAnchor = (anchor, rootObject) => {
+  if(!rootObject || !anchor || R.isEmpty(rootObject)) return undefined;
+  const anchorParts = anchor.split('.');
+  let currentElement = rootObject;
+
+  for(const anchorPart of anchorParts) {
+    currentElement = findAnchoredCategoryOrComponentFromElement(anchorPart, currentElement);
+
+    if(!currentElement) {
+      return undefined;
+    }
+  }
+
+  return currentElement;
+};
+
 export const removeAnchorPart = (anchor, index) => {
   return R.compose(
     R.join("."),
