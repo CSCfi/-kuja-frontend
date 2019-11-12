b3be7200e4fe1e445b1d55dde18593019f35050c
JP Yliuntinen <juho-petteri.yliuntinen@valamis.com>
2019-11-12 09:45:10 +0200

    Refactor and augment saving actions to support sending

--- src/services/muutoshakemus/actions.js
+++ src/services/muutoshakemus/actions.js
@@ -9,7 +9,10 @@ import {
   SET_SECTION_DATA,
   DOWNLOAD_ATTACHMENT_START,
   DOWNLOAD_ATTACHMENT_SUCCESS,
-  DOWNLOAD_ATTACHMENT_FAILURE
+  DOWNLOAD_ATTACHMENT_FAILURE,
+  SEND_MUUTOSPYYNTO_START,
+  SEND_MUUTOSPYYNTO_SUCCESS,
+  SEND_MUUTOSPYYNTO_FAILURE
 } from "./actionTypes";
 
 export const setBackendChanges = changes => {
@@ -30,9 +33,9 @@ export const setSectionData = payload => {
   };
 };
 
-export function saveMuutospyynto(muutospyynto, attachments, triggerPreview = false) {
+const createMuutospyyntoOutput = (muutospyynto, attachments) => {
   let data = new FormData();
-  var muutos = new Blob([JSON.stringify(muutospyynto)], {
+  const muutos = new Blob([JSON.stringify(muutospyynto)], {
     type: "application/json"
   });
   data.append("muutospyynto", muutos, "muutospyynnön json-data");
@@ -46,20 +49,47 @@ export function saveMuutospyynto(muutospyynto, attachments, triggerPreview = fal
       return null;
     });
   }
-  return dispatch => {
-    dispatch({ type: SAVE_MUUTOSPYYNTO_START });
+  return data;
+}
 
-    return axios
-      .post(`${API_BASE_URL}/muutospyynnot/tallenna`, data, {
-        withCredentials: true
-      })
-      .then(response => {
-        dispatch({ type: SAVE_MUUTOSPYYNTO_SUCCESS, payload: { response, triggerPreview }});
-      })
-      .catch(err => {
-        dispatch({ type: SAVE_MUUTOSPYYNTO_FAILURE, payload: err });
-      });
-  };
+const save = (data) => {
+  return axios.post(`${API_BASE_URL}/muutospyynnot/tallenna`, data, {
+    withCredentials: true
+  });
+}
+
+const send = (uuid) => {
+  return axios
+    .post(`${API_BASE_URL}/muutospyynnot/tila/avoin/${uuid}`,{}, {
+      withCredentials: true
+    })
+}
+
+export const saveMuutospyynto = (muutospyynto, attachments, triggerPreview = false) => async (dispatch) => {
+  const data = createMuutospyyntoOutput(muutospyynto, attachments);
+  dispatch({ type: SAVE_MUUTOSPYYNTO_START });
+
+  try {
+    const response = await save(data);
+    dispatch({ type: SAVE_MUUTOSPYYNTO_SUCCESS, payload: { response, triggerPreview }});
+  }
+  catch(err) {
+    dispatch({ type: SAVE_MUUTOSPYYNTO_FAILURE, payload: err });
+  }
+}
+
+export const saveAndSendMuutospyynto = (muutospyynto, attachments) => async (dispatch) => {
+  const data = createMuutospyyntoOutput(muutospyynto, attachments);
+  dispatch({ type: SEND_MUUTOSPYYNTO_START});
+
+  try {
+    const intermediateResponse = await save(data);
+    await send(intermediateResponse.data.uuid);
+    dispatch({type: SEND_MUUTOSPYYNTO_SUCCESS})
+  }
+  catch(err) {
+    dispatch({type: SEND_MUUTOSPYYNTO_FAILURE})
+  }
 }
 
 export function downloadAttachment(uuid) {
