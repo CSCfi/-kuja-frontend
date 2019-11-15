ce202398a3039bfa3b6197b6b15b3be84a160021
Raimo Pitkänen <rapitkan@gmail.com>
2019-11-15 18:20:47 +0200

    FileUpload component created.

---
+++ src/components/02-organisms/FileUpload/index.js
@@ -0,0 +1,296 @@
+import React, { useCallback, useEffect, useState } from "react";
+import { useDropzone } from "react-dropzone";
+import PropTypes from "prop-types";
+import * as R from "ramda";
+import ContentEditable from "react-contenteditable";
+import { FaTimes, FaLock, FaUnlock, FaDownload } from "react-icons/fa";
+import common from "../../../i18n/definitions/common";
+import { injectIntl } from "react-intl";
+import ToggleButton from "@material-ui/lab/ToggleButton";
+import { withStyles, IconButton } from "@material-ui/core";
+import Tooltip from "../Tooltip";
+import { downloadFileFn } from "../../../utils/common";
+
+const alter = R.curry((id, value, path, items) =>
+  R.map(R.when(R.propEq("id", id), R.assocPath(path, value)), items)
+);
+
+const StyledToggleButton = withStyles({
+  root: {
+    width: "3em",
+    height: "3em",
+    marginTop: "0.5em",
+    marginBottom: "0.5em",
+    "&.Mui-selected": {
+      color: "red"
+    }
+  }
+})(ToggleButton);
+
+const defaultProps = {
+  acceptedTypes: ["image/*", "application/pdf"],
+  minSize: 0,
+  maxSize: 26214400 // 1MB = 1048576
+};
+
+const FileUpload = ({
+  acceptedTypes = defaultProps.acceptedTypes,
+  intl,
+  minSize = defaultProps.minSize,
+  maxSize = defaultProps.maxSize,
+  onChanges,
+  payload,
+  uploadedFiles = []
+}) => {
+  const [acceptedFilesCount, setAcceptedFilesCount] = useState([]);
+  const [readFiles, setReadFiles] = useState([]);
+
+  function readFile(file) {
+    const reader = new FileReader();
+    reader.onabort = () => console.log("file reading was aborted");
+    reader.onerror = () => console.log("file reading has failed");
+    reader.onload = () => {
+      const binaryStr = reader.result;
+      setReadFiles(prevState => {
+        return [
+          ...prevState,
+          {
+            properties: file
+          }
+        ];
+      });
+    };
+    reader.readAsBinaryString(file);
+    return reader;
+  }
+
+  const onDrop = useCallback(
+    acceptedFiles => {
+      if (acceptedFiles.length > 0) {
+        const files = R.concat(
+          uploadedFiles,
+          R.map(file => {
+            const liite = {
+              tiedostoId: Math.random() + "-" + file.name,
+              filename: file.name,
+              kieli: "fi", // Hard coded... but why?
+              name: file.name,
+              nimi: file.name.toLowerCase(),
+              tiedosto: new Blob([file]),
+              koko: file.size,
+              size: file.size,
+              tyyppi: R.last(R.split("/", file.type)),
+              type: file.type,
+              removed: false,
+              salainen: false,
+              new: true
+            };
+            return {
+              file: liite,
+              id: Math.random()
+            };
+          }, acceptedFiles)
+        );
+        onChanges(payload, {
+          files
+        });
+      }
+      // setAcceptedFilesCount(acceptedFiles.length);
+      // R.forEach(file => readFile(file), acceptedFiles);
+    },
+    [uploadedFiles]
+  );
+
+  const {
+    isDragActive,
+    getRootProps,
+    getInputProps,
+    rejectedFiles
+  } = useDropzone({
+    accept: acceptedTypes,
+    minSize,
+    maxSize,
+    onDrop
+  });
+
+  useEffect(() => {
+    if (readFiles.length === acceptedFilesCount) {
+      onChanges(payload, {
+        files: readFiles
+      });
+    }
+  }, [readFiles]);
+
+  function handleFileRename(e, id) {
+    onChanges(payload, {
+      files: alter(id, e.target.value, ["file", "name"], uploadedFiles)
+    });
+  }
+
+  function togglePublicity(id, newValue) {
+    onChanges(payload, {
+      files: alter(id, newValue, ["file", "salainen"], uploadedFiles)
+    });
+  }
+
+  function removeFile(id) {
+    onChanges(payload, {
+      files: R.filter(
+        R.compose(
+          R.not,
+          R.equals(id),
+          R.prop("id")
+        ),
+        uploadedFiles
+      )
+    });
+  }
+
+  function downloadFile(file) {
+    const fileToDownload = file.uuid
+      ? { ...file, url: `/liitteet/${file.uuid}/raw` }
+      : file;
+    downloadFileFn(fileToDownload)();
+  }
+
+  //   useEffect(() => {
+  //     if (rejectedFiles.length > 0) {
+  //       let message = "Jokin tai jotkin tiedostoista ovat liian suuria.";
+  //       if (rejectedFiles.length === 1) {
+  //         message = `Tiedoston lisääminen epäonnistui.`;
+  //       }
+  //       toast.error(
+  // `${message} Yksittäisen tiedoston koko saa olla korkeintaan ${(0.0009765625 /
+  //   1000) *
+  //   maxSize} MB.`,
+  //         {
+  //           autoClose: 5000,
+  //           position: toast.POSITION.BOTTOM_CENTER
+  //         }
+  //       );
+  //     }
+  //   }, [rejectedFiles]);
+
+  return (
+    <div>
+      <div {...getRootProps()}>
+        <input {...getInputProps()} />
+        <div className="h-20 cursor-pointer flex flex-col justify-center items-center border border-dashed border-gray-600">
+          {isDragActive ? (
+            <p>Pudota tiedostot tähän ...</p>
+          ) : (
+            <React.Fragment>
+              <p>
+                Voit raahata tiedostot tähän tai klikata aluetta lisätäksesi
+                liitteitä.
+              </p>
+              {uploadedFiles.length > 0 && <p>Liitteet näkyvät alla.</p>}
+            </React.Fragment>
+          )}
+        </div>
+      </div>
+      <ul className="mt-4">
+        {R.addIndex(R.map)(({ file, id }, i) => {
+          return (
+            <li
+              key={`file-${id}`}
+              className={i > 0 ? "border-t border-gray-400" : ""}
+            >
+              <span className="flex items-center">
+                <span className="w-5/12 pr-2">
+                  <ContentEditable
+                    html={file.name} // innerHTML of the editable div
+                    disabled={false} // use true to disable edition
+                    onChange={e => handleFileRename(e, id)} // handle innerHTML change
+                    tagName="span"
+                  />
+                </span>
+                <span className="w-2/12">{file.type}</span>
+                <span className="w-2/12">{Math.round(file.size) / 100} KB</span>
+                <span className="w-1/12">
+                  {file.uuid && (
+                    <Tooltip
+                      placement="left"
+                      tooltip="Tallenna liite koneellesi"
+                    >
+                      <IconButton
+                        variant="contained"
+                        size="small"
+                        onClick={() => downloadFile(file)}
+                      >
+                        <FaDownload />
+                      </IconButton>
+                    </Tooltip>
+                  )}
+                </span>
+                <span className="w-1/12">
+                  <Tooltip
+                    placement="bottom"
+                    tooltip={
+                      file.salainen
+                        ? intl.formatMessage(common.attachmentSecretUnselect)
+                        : intl.formatMessage(common.attachmentSecretSelect)
+                    }
+                  >
+                    <StyledToggleButton
+                      value="check"
+                      selected={file.salainen}
+                      size="small"
+                      onChange={() => {
+                        togglePublicity(id, !!!file.salainen);
+                      }}
+                      title={
+                        file.salainen
+                          ? intl.formatMessage(common.attachmentSecretUnselect)
+                          : intl.formatMessage(common.attachmentSecretSelect)
+                      }
+                    >
+                      {!file.salainen ? <FaUnlock /> : <FaLock />}
+                    </StyledToggleButton>
+                  </Tooltip>
+                </span>
+                <span className="w-1/12">
+                  <Tooltip
+                    placement="right"
+                    tooltip="Poista liite hakemukselta"
+                  >
+                    <IconButton
+                      variant="contained"
+                      size="small"
+                      onClick={() => removeFile(id)}
+                    >
+                      <FaTimes />
+                    </IconButton>
+                  </Tooltip>
+                </span>
+              </span>
+            </li>
+          );
+        }, uploadedFiles)}
+      </ul>
+      {rejectedFiles.length > 0 && (
+        <div>
+          <span className="text-red-600">
+            {rejectedFiles.length === 1 &&
+              `Tiedosto oli ${rejectedFiles[0].name} liian suuri ladattavaksi.`}
+            {rejectedFiles.length > 1 &&
+              "Osa tiedostoista oli liian suuria ladattaviksi."}{" "}
+            Suurin sallittu tiedostokoko on {(0.0009765625 / 1000) * maxSize}{" "}
+            MB.
+          </span>
+        </div>
+      )}
+    </div>
+  );
+};
+
+FileUpload.propTypes = {
+  minSize: PropTypes.number,
+  maxSize: PropTypes.number,
+  acceptedTypes: PropTypes.array,
+  onChanges: PropTypes.func.isRequired,
+  payload: PropTypes.object,
+  uploadedFiles: PropTypes.array
+};
+
+export default injectIntl(FileUpload);
