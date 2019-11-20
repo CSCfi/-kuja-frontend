3b6462e0e6241c6d00c1218c97c9bb288ed4a597
Raimo Pitkänen <rapitkan@gmail.com>
2019-11-20 09:59:37 +0200

    Bug fix. 3 methods for getting the reasoning related changes.

--- src/services/muutoshakemus/utils/changes-to-save.js
+++ src/services/muutoshakemus/utils/changes-to-save.js
@@ -1,5 +1,5 @@
 import { getMetadata } from "./tutkinnotUtils";
-import { getAnchorPart, findAnchoredElement } from "../../../utils/common";
+import { getAnchorPart } from "../../../utils/common";
 import * as R from "ramda";
 
 const getMuutos = (stateItem, changeObj, perustelut) => {
@@ -66,6 +66,48 @@ const getAnchorBase = (key, anchorInit) => {
   return anchorBase;
 };
 
+function findBackendMuutos(anchor, backendMuutokset) {
+  const backendMuutos = R.find(muutos => {
+    return !!R.find(R.propEq("anchor", anchor), muutos.meta.changeObjects);
+  }, backendMuutokset);
+  if (!backendMuutos && R.includes(".", anchor)) {
+    return findBackendMuutos(
+      R.compose(R.join("."), R.init, R.split("."), R.always(anchor)),
+      backendMuutokset
+    );
+  }
+  return { anchor, backendMuutos };
+}
+
+/**
+ * Function returns change objects related to reasoning (perustelut)
+ * and to current anchor. There are different methods to find the
+ * correct change objects.
+ * @param {string} anchor - Dot separated string, id of a change object.
+ * @param {array} changeObjects - Array of change objects.
+ */
+function findPerustelut(anchor, changeObjects) {
+  function getPerustelut(anchor, perustelut, method = R.includes) {
+    return R.filter(R.compose(method(anchor), R.prop("anchor")), perustelut);
+  }
+  // Method 1: Add perustelut_ string in front of the anchor.
+  let perustelutAnchor = `perustelut_${anchor}`;
+  let perustelut = getPerustelut(perustelutAnchor, changeObjects);
+  if (R.isEmpty(perustelut)) {
+    // Method 2: Remove the last part of the anchor and try method 1 again.
+    let anchorInit = R.slice(0, R.lastIndexOf(".", anchor), anchor);
+    perustelutAnchor = `perustelut_${anchorInit}`;
+    perustelut = getPerustelut(perustelutAnchor, changeObjects);
+    if (R.isEmpty(perustelut)) {
+      // Method 3: Take the anchor of method to and replace the dots with
+      // underscores.
+      perustelutAnchor = `perustelut_${R.replace(".", "_", anchorInit)}`;
+      perustelut = getPerustelut(perustelutAnchor, changeObjects, R.startsWith);
+    }
+  }
+  return perustelut;
+}
+
 export const getChangesToSave = (
   key,
   stateObject = {},
@@ -73,28 +115,12 @@ export const getChangesToSave = (
   backendMuutokset = []
 ) => {
   const paivitetytBackendMuutokset = R.map(changeObj => {
-    const anchorInit = R.compose(
-      R.join("."),
-      R.init,
-      R.split("."),
-      R.prop("anchor")
-    )(changeObj);
-    const anchorBase = getAnchorBase(key, anchorInit);
-    const backendMuutos = R.find(muutos => {
-      return !!R.find(
-        R.startsWith(anchorInit),
-        R.map(
-          R.compose(R.join("."), R.init, R.split("."), R.prop("anchor")),
-          R.path(["meta", "changeObjects"], muutos)
-        )
-      );
-    }, backendMuutokset);
+    let { anchor, backendMuutos } = findBackendMuutos(
+      changeObj.anchor,
+      backendMuutokset
+    );
     if (backendMuutos) {
-      const perustelutAnchorInitial = `perustelut_${anchorBase}`;
-      const perustelut = R.filter(
-        R.compose(R.contains(perustelutAnchorInitial), R.prop("anchor")),
-        changeObjects.perustelut
-      );
+      const perustelut = findPerustelut(anchor, changeObjects.perustelut);
       return R.assocPath(
         ["meta", "changeObjects"],
         R.flatten([[changeObj], perustelut]),
