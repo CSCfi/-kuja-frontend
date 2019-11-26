9a911c8062ef4ea5ca570b39e99d3c9b29c80e50
Raimo Pitkänen <rapitkan@gmail.com>
2019-11-26 10:09:47 +0200

    Click handler on table rows.

--- src/components/02-organisms/Table2/TableRow/index.js
+++ src/components/02-organisms/Table2/TableRow/index.js
@@ -1,8 +1,20 @@
 import React from "react";
 
-const TableRow = ({ children }) => {
+const TableRow = ({ children, properties }) => {
+  function onRowClick(action = "click") {
+    if (properties.onClick) {
+      properties.onClick(action, properties);
+    }
+  }
+
   return (
-    <div key={`key-${Math.random()}`} role="row" className="flex">
+    <div
+      key={`key-${Math.random()}`}
+      role="row"
+      className="flex cursor-pointer hover:bg-gray-100"
+      onClick={() => {
+        onRowClick();
+      }}>
       {children}
     </div>
   );
