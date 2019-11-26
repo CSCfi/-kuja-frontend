88dab30382c4b0e9e649e77fa0d6269a728ed427
Raimo Pitkänen <rapitkan@gmail.com>
2019-11-26 18:44:23 +0200

    SimpleMenu component created (for table actions).

---
+++ src/components/02-organisms/SimpleMenu/index.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import { IconButton } from "@material-ui/core";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import PropTypes from "prop-types";
+import * as R from "ramda";
+
+function SimpleMenu({ actions }) {
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = action => {
+    if (action) {
+      if (action.onClick) {
+        action.onClick(action);
+      }
+    }
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <IconButton
+        aria-controls="simple-menu"
+        aria-haspopup="true"
+        onClick={handleClick}>
+        <MoreVertIcon />
+      </IconButton>
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={() => handleClose()}>
+        {R.addIndex(R.map)((action, i) => {
+          return (
+            <MenuItem
+              key={`action-${i}`}
+              onClick={e => {
+                e.preventDefault();
+                handleClose(action);
+                return false;
+              }}>
+              {action.text}
+            </MenuItem>
+          );
+        }, actions)}
+      </Menu>
+    </div>
+  );
+}
+
+SimpleMenu.propTypes = {
+  actions: PropTypes.array
+};
+
+export default SimpleMenu;
