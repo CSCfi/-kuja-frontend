28ed8c0e50dbbd59fd715c0595ead8fc4b2503b6
Raimo Pitkänen <rapitkan@gmail.com>
2019-12-09 15:57:59 +0200

    Unneeded variables removed.

--- tests/e2e/tests/muutoshakemus.js
+++ tests/e2e/tests/muutoshakemus.js
@@ -5,19 +5,10 @@ const R = require("ramda");
 
 const {
   $,
-  button,
-  goto,
   click,
-  closeBrowser,
-  currentURL,
   focus,
-  inputField,
-  into,
-  link,
-  openBrowser,
   scrollDown,
   scrollTo,
-  text,
   textBox,
   waitFor,
   write
