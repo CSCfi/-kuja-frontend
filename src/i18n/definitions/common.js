91875455099cfa581dbe22f946946277b43b814d
Raimo Pitkänen <rapitkan@gmail.com>
2019-12-20 13:41:03 +0200

    Duplicate translation removed.

--- src/i18n/definitions/common.js
+++ src/i18n/definitions/common.js
@@ -5,10 +5,6 @@ export default defineMessages({
     id: "common.statistics",
     defaultMessage: "Tilastot"
   },
-  current: {
-    id: "common.current",
-    defaultMessage: "Nykyinen"
-  },
   applyFor: {
     id: "common.applyFor",
     defaultMessage: "Haettava"
