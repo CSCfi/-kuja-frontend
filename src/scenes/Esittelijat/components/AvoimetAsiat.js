46e3e012d36d2af46c03601b648b003391c60c19
JP Yliuntinen <juho-petteri.yliuntinen@valamis.com>
2020-01-09 14:24:49 +0200

    Remove the use of FetchHandler and backendContext

--- src/scenes/Esittelijat/components/AvoimetAsiat.js
+++ src/scenes/Esittelijat/components/AvoimetAsiat.js
@@ -1,28 +1,13 @@
-import FetchHandler from "../../../FetchHandler";
-import React, {useContext, useMemo} from "react";
-import {BackendContext} from "../../../context/backendContext";
+import React from "react";
 import Table from "../../../components/02-organisms/Table"
-import * as R from "ramda";
 import {generateAsiatTableStructure} from "../modules/asiatUtils";
 import {useIntl} from "react-intl";
 import {PropTypes} from "prop-types";
 
 const AvoimetAsiat = () => {
     const intl = useIntl();
-    const {state: fromBackend, dispatch} = useContext(BackendContext);
-    const fetchSetup = useMemo(() => {
-      const fetchItems = [];
-      fetchItems.push({
-        key: "muutospyynnot",
-        dispatchFn: dispatch,
-        urlEnding: "avoimet"
-      });
-      return fetchItems;
-    }, [dispatch]);
 
-    const avoimetAsiatData = useMemo(() => {
-      return R.path(["muutospyynnot", "raw"], fromBackend);
-    }, [fromBackend]);
+    const avoimetAsiatData = [];
 
     const tableStructure = !!avoimetAsiatData ?
       generateAsiatTableStructure(avoimetAsiatData, intl.formatMessage) :
@@ -30,14 +15,9 @@ const AvoimetAsiat = () => {
 
     return (
       <React.Fragment>
-        <FetchHandler
-          fetchSetup={fetchSetup}
-          ready={
-            <React.Fragment>
-              <Table structure={tableStructure}></Table>
-            </React.Fragment>
-          }
-        />
+        <React.Fragment>
+          <Table structure={tableStructure}></Table>
+        </React.Fragment>
       </React.Fragment>
     )
   }
