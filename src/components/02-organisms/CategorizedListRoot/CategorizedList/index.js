f17626426f1e8ad00e2fde7cf8841872c2a4e9ef
JP Yliuntinen <juho-petteri.yliuntinen@valamis.com>
2020-01-28 20:27:52 +0200

    Replace SimpleButton component

--- src/components/02-organisms/CategorizedListRoot/CategorizedList/index.js
+++ src/components/02-organisms/CategorizedListRoot/CategorizedList/index.js
@@ -1,7 +1,7 @@
 import React, { useCallback } from "react";
 import PropTypes from "prop-types";
 import CheckboxWithLabel from "../../../01-molecules/CheckboxWithLabel";
-import SimpleButton from "../../../00-atoms/SimpleButton";
+import SimpleButton from "okm-frontend-components/dist/components/00-atoms/SimpleButton";
 import Dropdown from "okm-frontend-components/dist/components/00-atoms/Dropdown";
 import RadioButtonWithLabel from "../../../01-molecules/RadioButtonWithLabel";
 import Input from "okm-frontend-components/dist/components/00-atoms/Input";
