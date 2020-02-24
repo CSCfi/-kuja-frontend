088fd3f4918a9786afe4c6e575f4d0d95d51ab02
JP Yliuntinen <juho-petteri.yliuntinen@valamis.com>
2020-02-24 15:49:16 +0200

    Replace some styled components with tailwind classes

--- src/scenes/VapaaSivistystyo/components/JarjestajaBasicInfo.js
+++ src/scenes/VapaaSivistystyo/components/JarjestajaBasicInfo.js
@@ -1,22 +1,12 @@
 import React from "react";
 import styled from "styled-components";
 
-const OrganizerH1 = styled.h1`
-  margin-bottom: 10px;
-`;
-
-const LargeParagraph = styled.p`
-  font-size: 20px;
-  line-height: 24px;
-  margin: 0;
-`;
-
 const JarjestajaBasicInfo = ({ jarjestaja }) => {
   return (
-    <React.Fragment>
-      <OrganizerH1>{jarjestaja.nimi}</OrganizerH1>
-      <LargeParagraph>{jarjestaja.ytunnus}</LargeParagraph>
-    </React.Fragment>
+    <>
+      <h1 className="mb-1">{jarjestaja.nimi}</h1>
+      <p className="text-xl leading-tight m-0">{jarjestaja.ytunnus}</p>
+    </>
   );
 };
 
