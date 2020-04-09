df43f55a4575846027664d08d203b78f05413492
JP Yliuntinen <juho-petteri.yliuntinen@valamis.com>
2020-04-09 09:32:10 +0300

    refactor: remove unused utils

--- src/modules/helpers.test.js
+++ src/modules/helpers.test.js
@@ -1,4 +1,4 @@
-import { resolveKoodiLocalization, parsePostalCode, slugify } from "./helpers";
+import { resolveKoodiLocalization } from "./helpers";
 
 it("should return undefined", () => {
   const result = resolveKoodiLocalization({}, "FI", "nimi", "kieli");
@@ -44,23 +44,4 @@ it("should work with defaults", () => {
   ];
   const result = resolveKoodiLocalization(obj);
   expect(result).toBe("Test FI");
-});
-
-it("should slugify", () => {
-  const str = "Example text -- ^åbo 2é";
-  expect(slugify(str)).toBe("example-text-bo-2e");
-});
-
-it("should return the original value", () => {
-  expect(slugify(false)).toBe(false);
-});
-
-it("should return an empty string", () => {
-  const postalCode = null;
-  expect(parsePostalCode(postalCode)).toBe("");
-});
-
-it("should parse a postal code", () => {
-  const postalCode = "FI-40100";
-  expect(parsePostalCode(postalCode)).toBe("40100");
 });
