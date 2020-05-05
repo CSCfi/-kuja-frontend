ebc6af392be46f8d950ba8e9772854fa0b645941
JP Yliuntinen <juho-petteri.yliuntinen@valamis.com>
2020-05-05 15:39:03 +0300

    feat: CSCKUJA-410 remove authentication features

--- src/scenes/Login/services/RequireCasAuth.js
+++
@@ -1,3 +0,0 @@
-import { CAS_LOGIN_REDIRECT_URL } from '../../../modules/constants'
-
-export default () => window.location = CAS_LOGIN_REDIRECT_URL
--- src/scenes/Logout/services/DestroyCasAuth.js
+++
@@ -1,3 +0,0 @@
-import {CAS_LOGOUT_URL} from "../../../modules/constants";
-
-export default () => window.location = CAS_LOGOUT_URL
