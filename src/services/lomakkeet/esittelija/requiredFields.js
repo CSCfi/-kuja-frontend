ad67cceac2960a59f2cde761b3b66d8940d5f924
Raimo Pitkänen <rapitkan@gmail.com>
2020-05-12 14:01:23 +0300

    feat: custom validation rule for asianumero field set (#432)

--- src/services/lomakkeet/esittelija/requiredFields.js
+++ src/services/lomakkeet/esittelija/requiredFields.js
@@ -1,5 +1,4 @@
 export const requiredFields = [
-  { anchor: "asianumero.A" },
   { anchor: "paatospaiva.A" },
   { anchor: "voimaantulopaiva.A" }
 ];
--- src/services/lomakkeet/esittelija/rules.js
+++ src/services/lomakkeet/esittelija/rules.js
@@ -1,9 +1,50 @@
-import { createRules } from "../utils";
+import { createRules, ifOneTerm, getPathByAnchor } from "../utils";
 import { requiredFields } from "./requiredFields";
+import { assocPath, compose, concat, not, isEmpty } from "ramda";
 
 export function getRules(_lomake) {
   // Rules for top three fields of muutospyyntö
-  const rules = createRules(requiredFields);
+  const simpleRules = createRules(requiredFields);
 
-  return rules;
+  const advancedRules = [
+    {
+      isRequired: () => true,
+      markRequiredFields: lomake => {
+        const _path = getPathByAnchor(["asianumero", "A"], lomake);
+        return assocPath(
+          concat(_path, ["properties", "isRequired"]),
+          true,
+          lomake
+        );
+      },
+      isValid: (lomake, changeObjects) => {
+        return () =>
+          ifOneTerm(
+            [
+              {
+                anchor: "asianumero.A",
+                properties: {
+                  value: value => {
+                    const isValid = /^VN\/[0-9]{4,6}\/[0-9]{4}$/.test(value);
+                    return isValid;
+                  }
+                }
+              }
+            ],
+            lomake,
+            changeObjects
+          );
+      },
+      showErrors: (lomake, isValid) => {
+        const _path = getPathByAnchor(["asianumero", "A"], lomake);
+        return assocPath(
+          concat(_path, ["properties", "isValid"]),
+          isValid,
+          lomake
+        );
+      }
+    }
+  ];
+
+  return concat(simpleRules, advancedRules);
 }
