b64aba42a530933ead6da36eb215bbcd163a151c
JP Yliuntinen <juho-petteri.yliuntinen@valamis.com>
2020-05-28 09:03:31 +0300

    fix: CSCKUJA-377 use proper React.Fragment to allow messages extractor to run properly

--- src/scenes/VapaaSivistystyo/components/JarjestajaBasicInfo.js
+++ src/scenes/VapaaSivistystyo/components/JarjestajaBasicInfo.js
@@ -2,10 +2,10 @@ import React from "react";
 
 const JarjestajaBasicInfo = ({ jarjestaja }) => {
   return (
-    <>
+    <React.Fragment>
       <h1 className="mb-1">{jarjestaja.nimi}</h1>
       <p className="text-xl leading-tight m-0">{jarjestaja.ytunnus}</p>
-    </>
+    </React.Fragment>
   );
 };
 
