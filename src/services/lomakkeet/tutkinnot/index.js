46e7af93a3bb6b72d2a7cc0bb5efcc372c63150f
Raimo Pitkänen <rapitkan@gmail.com>
2020-06-12 15:41:07 +0300

    fix: tutkinnot and osaamisalat form didn't update when needed (#458)

--- src/services/lomakkeet/tutkinnot/index.js
+++ src/services/lomakkeet/tutkinnot/index.js
@@ -3,144 +3,121 @@ import _ from "lodash";
 import { isAdded, isRemoved, isInLupa } from "../../../css/label";
 import { findObjectWithKey } from "../../../utils/common";
 
-const categories = {};
+function getModificationForm(article, koulutustyypit, title, areaCode, locale) {
+  return R.values(
+    R.addIndex(R.map)((koulutustyyppi, i) => {
+      return {
+        anchor: koulutustyyppi.koodiArvo,
+        meta: {
+          areaCode,
+          title
+        },
+        title: _.find(koulutustyyppi.metadata, m => {
+          return m.kieli === locale;
+        }).nimi,
+        categories: R.addIndex(R.map)((koulutus, ii) => {
+          const maaraysKoulutukselle = article
+            ? R.find(
+                R.propEq("koodi", koulutus.koodiArvo),
+                findObjectWithKey(article.koulutusalat, "koulutukset")
+              ) || {}
+            : {};
 
-function getModificationForm(
-  index,
-  article,
-  koulutustyypit,
-  title,
-  areaCode,
-  locale
-) {
-  function getFormStructure() {
-    return R.values(
-      R.addIndex(R.map)((koulutustyyppi, i) => {
-        return {
-          anchor: koulutustyyppi.koodiArvo,
-          meta: {
-            areaCode,
-            title
-          },
-          title: _.find(koulutustyyppi.metadata, m => {
-            return m.kieli === locale;
-          }).nimi,
-          categories: R.addIndex(R.map)((koulutus, ii) => {
-            const maaraysKoulutukselle = article
+          const isTutkintoInLupaBool = !R.isEmpty(maaraysKoulutukselle);
+
+          let isTutkintoIndeterminate = false;
+
+          const osaamisalat = (koulutus.osaamisalat || []).map(osaamisala => {
+            const maaraysOsaamisalalle = article
               ? R.find(
-                  R.propEq("koodi", koulutus.koodiArvo),
-                  findObjectWithKey(article.koulutusalat, "koulutukset")
+                  R.propEq("koodi", osaamisala.koodiArvo),
+                  findObjectWithKey(article.koulutusalat, "rajoitteet")
                 ) || {}
               : {};
-
-            const isTutkintoInLupaBool = !R.isEmpty(maaraysKoulutukselle);
-
-            let isTutkintoIndeterminate = false;
-
-            const osaamisalat = (koulutus.osaamisalat || []).map(osaamisala => {
-              const maaraysOsaamisalalle = article
-                ? R.find(
-                    R.propEq("koodi", osaamisala.koodiArvo),
-                    findObjectWithKey(article.koulutusalat, "rajoitteet")
-                  ) || {}
-                : {};
-              const osaamisalamaaraysFound = !R.isEmpty(maaraysOsaamisalalle);
-              const isOsaamisalaInLupaBool =
-                isTutkintoInLupaBool && !osaamisalamaaraysFound;
-              if (!isOsaamisalaInLupaBool) {
-                isTutkintoIndeterminate = true;
-              }
-              return {
-                anchor: osaamisala.koodiArvo,
-                components: [
-                  {
-                    anchor: "osaamisala",
-                    name: "CheckboxWithLabel",
-                    properties: {
-                      forChangeObject: {
-                        maaraysUuid: maaraysOsaamisalalle.maaraysId,
-                        koodisto: osaamisala.koodisto,
-                        metadata: osaamisala.metadata,
-                        isInLupa: isOsaamisalaInLupaBool,
-                        isTutkintoInLupa: isTutkintoInLupaBool
-                      },
-                      name: "CheckboxWithLabel",
-                      code: osaamisala.koodiArvo,
-                      title: _.find(osaamisala.metadata, m => {
-                        return m.kieli === locale;
-                      }).nimi,
-                      labelStyles: {
-                        addition: isAdded,
-                        removal: isRemoved,
-                        custom: Object.assign(
-                          {},
-                          isOsaamisalaInLupaBool ? isInLupa : {}
-                        )
-                      },
-                      isChecked: isOsaamisalaInLupaBool
-                    }
-                  }
-                ]
-              };
-            });
-
+            const osaamisalamaaraysFound = !R.isEmpty(maaraysOsaamisalalle);
+            const isOsaamisalaInLupaBool =
+              isTutkintoInLupaBool && !osaamisalamaaraysFound;
+            if (!isOsaamisalaInLupaBool) {
+              isTutkintoIndeterminate = true;
+            }
             return {
-              anchor: koulutus.koodiArvo,
+              anchor: osaamisala.koodiArvo,
               components: [
                 {
-                  anchor: "tutkinto",
+                  anchor: "osaamisala",
                   name: "CheckboxWithLabel",
                   properties: {
                     forChangeObject: {
-                      maaraysUuid: maaraysKoulutukselle.maaraysId,
-                      koodisto: koulutus.koodisto,
-                      metadata: koulutus.metadata,
-                      isInLupa: isTutkintoInLupaBool
+                      maaraysUuid: maaraysOsaamisalalle.maaraysId,
+                      koodisto: osaamisala.koodisto,
+                      metadata: osaamisala.metadata,
+                      isInLupa: isOsaamisalaInLupaBool,
+                      isTutkintoInLupa: isTutkintoInLupaBool
                     },
                     name: "CheckboxWithLabel",
-                    code: koulutus.koodiArvo,
-                    title:
-                      _.find(koulutus.metadata, m => {
-                        return m.kieli === locale;
-                      }).nimi || "[Koulutuksen otsikko tähän]",
+                    code: osaamisala.koodiArvo,
+                    title: _.find(osaamisala.metadata, m => {
+                      return m.kieli === locale;
+                    }).nimi,
                     labelStyles: {
                       addition: isAdded,
                       removal: isRemoved,
                       custom: Object.assign(
                         {},
-                        isTutkintoInLupaBool ? isInLupa : {}
+                        isOsaamisalaInLupaBool ? isInLupa : {}
                       )
                     },
-                    isChecked: isTutkintoInLupaBool,
-                    isIndeterminate: isTutkintoIndeterminate
+                    isChecked: isOsaamisalaInLupaBool
                   }
                 }
-              ],
-              categories: osaamisalat
+              ]
             };
-          }, koulutustyyppi.koulutukset)
-        };
-      }, koulutustyypit)
-    );
-  }
+          });
 
-  if (index) {
-    if (categories[index]) {
-      return categories[index];
-    } else {
-      categories[index] = getFormStructure();
-      return categories[index];
-    }
-  } else {
-    return getFormStructure();
-  }
+          return {
+            anchor: koulutus.koodiArvo,
+            components: [
+              {
+                anchor: "tutkinto",
+                name: "CheckboxWithLabel",
+                properties: {
+                  forChangeObject: {
+                    maaraysUuid: maaraysKoulutukselle.maaraysId,
+                    koodisto: koulutus.koodisto,
+                    metadata: koulutus.metadata,
+                    isInLupa: isTutkintoInLupaBool
+                  },
+                  name: "CheckboxWithLabel",
+                  code: koulutus.koodiArvo,
+                  title:
+                    _.find(koulutus.metadata, m => {
+                      return m.kieli === locale;
+                    }).nimi || "[Koulutuksen otsikko tähän]",
+                  labelStyles: {
+                    addition: isAdded,
+                    removal: isRemoved,
+                    custom: Object.assign(
+                      {},
+                      isTutkintoInLupaBool ? isInLupa : {}
+                    )
+                  },
+                  isChecked: isTutkintoInLupaBool,
+                  isIndeterminate: isTutkintoIndeterminate
+                }
+              }
+            ],
+            categories: osaamisalat
+          };
+        }, koulutustyyppi.koulutukset)
+      };
+    }, koulutustyypit)
+  );
 }
 
 export default function getTutkinnotLomake(action, data, isReadOnly, locale) {
   switch (action) {
     case "modification":
       return getModificationForm(
-        data.index,
         data.article,
         data.koulutustyypit,
         data.title,
